616aee9227de68a11f10a8e9ab4e9f073088152c
juyeong1260 <juyeong1260@gmail.com>
2021-06-14 18:41:03 +0900

    fix: lint

--- packages/design-token/src/colors/index.ts
+++ packages/design-token/src/colors/index.ts
@@ -15,7 +15,7 @@ export const light: Readonly<ColorTheme> = Object.freeze({
     '$gray900': '#212124',
 
     // 메인컬러
-    '$carrot50': '#FFF5F0',
+    '$carrot50' : '#FFF5F0',
     '$carrot100': '#FFE2D2',
     '$carrot200': '#FFD2B9',
     '$carrot300': '#FFBC97',
@@ -24,21 +24,21 @@ export const light: Readonly<ColorTheme> = Object.freeze({
     '$carrot600': '#FA6616',
 
     // 주의
-    '$yellow50': '#FFF7E6',
+    '$yellow50' : '#FFF7E6',
     '$yellow500': '#FFC552',
     '$yellow800': '#CE6400',
 
     // 긍정, 성공
-    '$green50': '#E8FAF6',
+    '$green50' : '#E8FAF6',
     '$green500': '#00B493',
     '$green800': '#008C72',
 
     // 경고, 위험
-    '$red50': '#FFF3F2',
+    '$red50' : '#FFF3F2',
     '$red800': '#E81300',
 
     // 추가 링크
-    '$blue50': '#EBF7FA',
+    '$blue50' : '#EBF7FA',
     '$blue800': '#0A86B7',
   },
 
@@ -65,7 +65,7 @@ export const dark: Readonly<ColorTheme> = Object.freeze({
     '$gray900': '#EAEBEE',
 
     // 메인컬러
-    '$carrot50': '#EDE4E0',
+    '$carrot50' : '#EDE4E0',
     '$carrot100': '#EDD3C4',
     '$carrot200': '#EDC4AD',
     '$carrot300': '#EDB08E',
@@ -74,21 +74,21 @@ export const dark: Readonly<ColorTheme> = Object.freeze({
     '$carrot600': '#E96017',
 
     // 주의
-    '$yellow50': '#EDE6D6',
+    '$yellow50' : '#EDE6D6',
     '$yellow500': '#EDB84E',
     '$yellow800': '#C05F03',
 
     // 긍정, 성공
-    '$green50': '#D8E9E5',
+    '$green50' : '#D8E9E5',
     '$green500': '#03A88A',
     '$green800': '#03836C',
 
     // 경고, 위험
-    '$red50': '#EDE2E2',
+    '$red50' : '#EDE2E2',
     '$red800': '#D81403',
 
     // 추가 링크
-    '$blue50': '#DBE6E9',
+    '$blue50' : '#DBE6E9',
     '$blue800': '#0C7EAB',
   },
 
@@ -99,3 +99,4 @@ export const dark: Readonly<ColorTheme> = Object.freeze({
     divider: 'rgba(255, 255, 255, 0.2)'
   },
 });
+
