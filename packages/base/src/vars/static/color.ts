aef16313cf89e92b5699596d7165bf5d130f7537
Hyeseong Kim <tim@daangn.com>
2022-04-19 23:43:08 +0900

    refactor packages

---
+++ packages/base/src/vars/static/color.ts
@@ -0,0 +1,5 @@
+export const white = 'var(--seed-static-color-white)';
+export const black = 'var(--seed-static-color-black)';
+export const gray900 = 'var(--seed-static-color-gray-900)';
+export const carrot50 = 'var(--seed-static-color-carrot-50)';
+export const carrot800 = 'var(--seed-static-color-carrot-800)';
