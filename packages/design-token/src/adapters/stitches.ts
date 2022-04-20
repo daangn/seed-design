419081cd80ffea10218499d2acd30ec59f742cd5
Hyeseong Kim <tim@daangn.com>
2022-04-20 16:17:36 +0900

    fix syntax to avoid reserved keyword

--- packages/design-token/src/adapters/stitches.ts
+++ packages/design-token/src/adapters/stitches.ts
@@ -12,23 +12,23 @@ type Color = (
   | StaticColor
 );
 
-type SemanticColor = keyof TokenObject['semantic']['color'];
-type ScaleColor = `scale${Capitalize<keyof TokenObject['scale']['color']>}`;
-type StaticColor = `static${Capitalize<keyof TokenObject['scale']['color']>}`;
+type SemanticColor = keyof TokenObject['$semantic']['color'];
+type ScaleColor = `scale${Capitalize<keyof TokenObject['$scale']['color']>}`;
+type StaticColor = `static${Capitalize<keyof TokenObject['$scale']['color']>}`;
 
-export function toStitchesTheme(tokenObject: TokenObject): Theme {
+export function toStitchesTheme(vars: TokenObject): Theme {
   function capitalize(str: string) {
     return str.replace(/[ -_]./g, substr => substr[1].toUpperCase());
   }
 
   const colors = {};
-  for (const [key, value] of Object.entries(tokenObject.static)) {
+  for (const [key, value] of Object.entries(vars.$static)) {
     colors[`static${capitalize(key)}`] = value;
   }
-  for (const [key, value] of Object.entries(tokenObject.scale)) {
+  for (const [key, value] of Object.entries(vars.$scale)) {
     colors[`scale${capitalize(key)}`] = value;
   }
-  for (const [key, value] of Object.entries(tokenObject.semantic)) {
+  for (const [key, value] of Object.entries(vars.$semantic)) {
     colors[capitalize(key)] = value;
   }
 
