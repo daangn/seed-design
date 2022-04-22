4fd34d2581c6abc037943a9edfe4b03c0c099ac4
Asher <102206520+malangcat@users.noreply.github.com>
2022-04-22 19:25:12 +0900

    fix: missing overlay color tokens

--- packages/design-token/src/vars/static/color.ts
+++ packages/design-token/src/vars/static/color.ts
@@ -1,5 +1,7 @@
 export const white = 'var(--seed-static-color-white)';
 export const black = 'var(--seed-static-color-black)';
+export const blackAlpha200 = 'var(--seed-static-color-black-alpha-200)';
+export const blackAlpha500 = 'var(--seed-static-color-black-alpha-500)';
 export const gray900 = 'var(--seed-static-color-gray-900)';
 export const carrot50 = 'var(--seed-static-color-carrot-50)';
 export const carrot800 = 'var(--seed-static-color-carrot-800)';
