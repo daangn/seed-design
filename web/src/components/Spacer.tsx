27ecdcd2a537f192ec8619ab350f2eadb03a0e8f
junghyeonsu <jung660317@naver.com>
2022-10-20 16:25:59 +0900

    feat: Spacer 삭제 (h2 margin으로 대체)

--- web/src/components/Spacer.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-import React from "react";
-
-import * as style from "./Spacer.css";
-
-export default function Logo() {
-  return <div className={clsx(style.spacer)} />;
-}
