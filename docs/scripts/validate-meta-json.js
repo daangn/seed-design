20baff9f21a74d1a08d3e8ce00e0d1ffd4d0fcd6
junghyeonsu <jung660317@naver.com>
2023-01-20 15:30:03 +0900

    chore: test ci

--- docs/scripts/validate-meta-json.js
+++ docs/scripts/validate-meta-json.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 const ajv = new Ajv();
 
+console.log("Validating component meta json");
+
 function validateJsonInDir({ dir, validate, type }) {
   fs.readdir(dir, (err, files) => {
     if (err) {
