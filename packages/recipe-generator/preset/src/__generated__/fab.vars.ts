6649346c44203b9da3b127dd15878b5a8fcac05c
malangcat <asher@daangn.com>
2024-06-13 14:40:16 +0900

    fab spec

--- packages/recipe-generator/preset/src/__generated__/fab.vars.ts
+++ packages/recipe-generator/preset/src/__generated__/fab.vars.ts
@@ -1,1 +1,38 @@
-export const vars = {}
+export const vars = {
+  "base": {
+    "enabled": {
+      "root": {
+        "color": "var(--seed-color-bg-layer-floating)",
+        "borderRadius": "var(--seed-radii-full)"
+      },
+      "icon": {
+        "color": "var(--seed-color-fg-neutral)"
+      }
+    },
+    "pressed": {
+      "root": {
+        "color": "var(--seed-color-bg-layer-floating-pressed)"
+      }
+    }
+  },
+  "sizeSmall": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-10)"
+      },
+      "icon": {
+        "size": "var(--seed-unit-5)"
+      }
+    }
+  },
+  "sizeMedium": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-12)"
+      },
+      "icon": {
+        "size": "var(--seed-unit-6)"
+      }
+    }
+  }
+}
