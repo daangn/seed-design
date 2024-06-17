870e02ef9edfab27d0ac742afa6175b9b2e7bbaf
junghyeonsu <jung660317@naver.com>
2024-06-17 17:53:59 +0900

    chore: add aria-hidden in svg

--- docs/src/components/MainHeader.tsx
+++ docs/src/components/MainHeader.tsx
@@ -15,8 +15,8 @@ function GithubLogo() {
       viewBox="0 0 26 26"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden={true}
     >
-      <title>github</title>
       <path
         fillRule="evenodd"
         clipRule="evenodd"
