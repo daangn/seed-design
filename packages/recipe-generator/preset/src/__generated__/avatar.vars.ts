9b7fa646dd4dfb06235b8c089dd7c369c3464c2b
junghyeonsu <jung660317@naver.com>
2024-06-28 15:32:42 +0900

    Run build command for ignore vars file change

--- packages/recipe-generator/preset/src/__generated__/avatar.vars.ts
+++ packages/recipe-generator/preset/src/__generated__/avatar.vars.ts
@@ -1,85 +1,85 @@
 export const vars = {
-  base: {
-    enabled: {
-      root: {
-        cornerRadius: "var(--seed-radii-full)",
-        strokeColor: "var(--seed-color-stroke-decorative-subtle)",
-        strokeWidth: "1px",
+  "base": {
+    "enabled": {
+      "root": {
+        "cornerRadius": "var(--seed-radii-full)",
+        "strokeColor": "var(--seed-color-stroke-decorative-subtle)",
+        "strokeWidth": "1px"
       },
-      badge: {
-        cornerRadius: "var(--seed-radii-full)",
-        color: "var(--seed-color-bg-layer-1)",
-      },
-    },
+      "badge": {
+        "cornerRadius": "var(--seed-radii-full)",
+        "color": "var(--seed-color-bg-layer-1)"
+      }
+    }
   },
-  size20: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-5)",
+  "size20": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-5)"
       },
-      badge: {
-        size: "var(--seed-unit-2\\.5)",
-      },
-    },
+      "badge": {
+        "size": "var(--seed-unit-2\\.5)"
+      }
+    }
   },
-  size24: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-6)",
+  "size24": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-6)"
       },
-      badge: {
-        size: "var(--seed-unit-3)",
-      },
-    },
+      "badge": {
+        "size": "var(--seed-unit-3)"
+      }
+    }
   },
-  size36: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-9)",
+  "size36": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-9)"
       },
-      badge: {
-        size: "var(--seed-unit-4)",
-      },
-    },
+      "badge": {
+        "size": "var(--seed-unit-4)"
+      }
+    }
   },
-  size48: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-12)",
+  "size48": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-12)"
       },
-      badge: {
-        size: "var(--seed-unit-6)",
-      },
-    },
+      "badge": {
+        "size": "var(--seed-unit-6)"
+      }
+    }
   },
-  size64: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-16)",
+  "size64": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-16)"
       },
-      badge: {
-        size: "var(--seed-unit-6)",
-      },
-    },
+      "badge": {
+        "size": "var(--seed-unit-6)"
+      }
+    }
   },
-  size80: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-20)",
+  "size80": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-20)"
       },
-      badge: {
-        size: "var(--seed-unit-6)",
-      },
-    },
+      "badge": {
+        "size": "var(--seed-unit-6)"
+      }
+    }
   },
-  size96: {
-    enabled: {
-      root: {
-        size: "var(--seed-unit-24)",
+  "size96": {
+    "enabled": {
+      "root": {
+        "size": "var(--seed-unit-24)"
       },
-      badge: {
-        size: "var(--seed-unit-8)",
-      },
-    },
-  },
-};
+      "badge": {
+        "size": "var(--seed-unit-8)"
+      }
+    }
+  }
+}
