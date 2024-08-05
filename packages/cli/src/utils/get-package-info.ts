20ab8a03dcfed9934871826b5d9d72ed5306a2b7
junghyeonsu <jung660317@naver.com>
2024-08-05 15:31:05 +0900

    feat(CLI): change resolve path standard tsconfig to packagejson

--- packages/cli/src/utils/get-package-info.ts
+++ packages/cli/src/utils/get-package-info.ts
@@ -1,9 +1,17 @@
-import path from "path";
+import findup from "findup-sync";
 import fs from "fs-extra";
 import type { PackageJson } from "type-fest";
 
-export function getPackageInfo() {
-  const packageJsonPath = path.join("package.json");
+const PACKAGE_JSON = "package.json";
 
-  return fs.readJSONSync(packageJsonPath) as PackageJson;
+function getPackagePath() {
+  const packageJsonPath = findup(PACKAGE_JSON);
+  if (!packageJsonPath) {
+    throw new Error("No package.json file found in the project.");
+  }
+  return packageJsonPath;
+}
+
+export function getPackageInfo() {
+  return fs.readJSONSync(getPackagePath()) as PackageJson;
 }
