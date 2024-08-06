8bcf3777ebb0b0c4f3dc5a18f14d11fcdddf92aa
junghyeonsu <jung660317@naver.com>
2024-08-06 20:15:28 +0900

    chore: remove `@seed-design/design-token`

--- component-docs/stackflow/Stack.ts
+++ component-docs/stackflow/Stack.ts
@@ -1,4 +1,3 @@
-import { vars } from "@seed-design/design-token";
 import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
 import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
 import { ActivityComponentType, stackflow } from "@stackflow/react/future";
@@ -20,11 +19,7 @@ export const makeStack = (props: MakeStackProps) => {
       basicRendererPlugin(),
       basicUIPlugin({
         theme: "cupertino",
-        backgroundColor: vars.$semantic.color.paperDefault,
         appBar: {
-          textColor: vars.$scale.color.gray900,
-          iconColor: vars.$scale.color.gray900,
-          borderColor: vars.$semantic.color.divider3,
           backButton: {
             ariaLabel: "뒤로 가기",
           },
