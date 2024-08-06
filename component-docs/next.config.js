76bafba600f0bcc0aec96b4e8b35e00e81560439
junghyeonsu <jung660317@naver.com>
2024-08-06 20:26:54 +0900

    docs: add see

--- component-docs/next.config.js
+++ component-docs/next.config.js
@@ -17,6 +17,8 @@ const nextConfig = {
 
   /**
    * @stackflow/plugin-basic-ui 패키지 내부 코드를 강제로 cjs로 변환합니다.
+   * @see https://github.com/vercel/next.js/issues/39375
+   * @see https://github.com/vercel/next.js/issues/39375#issuecomment-1380266233
    */
   transpilePackages: ["@stackflow/plugin-basic-ui"],
 };
