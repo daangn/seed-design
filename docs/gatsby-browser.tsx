82c6fe8f671aa0b88e97e8da655b634eac6f3353
junghyeonsu <jung660317@naver.com>
2024-09-12 12:20:06 +0900

    docs: remove useEffect

--- docs/gatsby-browser.tsx
+++ docs/gatsby-browser.tsx
@@ -1,7 +1,6 @@
 import "./src/styles/global.css";
 
 import type { WrapPageElementBrowserArgs } from "gatsby";
-import { useEffect } from "react";
 
 import Layout from "./src/_Layout";
 import Root from "./src/_Root";
@@ -12,23 +11,6 @@ export const wrapPageElement = ({
 }: WrapPageElementBrowserArgs) => {
   const pathname = props.location.pathname;
   const layoutType = pathname === "/" ? "main" : "document";
-
-  useEffect(() => {
-    if (window.location.hostname === "seed-design.pages.dev") {
-      window.location.replace(`https://seed-design.io${pathname}`);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (
-      pathname.startsWith("/component") &&
-      pathname.endsWith("/") &&
-      !pathname.includes("usage") &&
-      !pathname.includes("style")
-    ) {
-      window.location.replace(`${pathname}usage/`);
-    }
-  }, []);
 
   return (
     <Root>
