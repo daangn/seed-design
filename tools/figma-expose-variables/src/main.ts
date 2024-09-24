1d968cee4c47c9369541f3f56bf5b05be4ff48fd
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-09-24 16:51:09 +0900

    feat: add `dynamic-page` property

--- tools/figma-expose-variables/src/main.ts
+++ tools/figma-expose-variables/src/main.ts
@@ -1,4 +1,8 @@
-import { COMBINATION_INPUT_FRAME_NAMES, MAIN_FRAME_RELAUNCH_DATA, SUFFIXES } from "./constants";
+import {
+  COMBINATION_INPUT_FRAME_NAMES,
+  MAIN_FRAME_RELAUNCH_DATA,
+  SUFFIXES,
+} from "./constants";
 import { createMainFrame, writeVariables } from "./utils";
 
 const firstSelection = figma.currentPage.selection[0];
@@ -8,29 +12,36 @@ const isFirstSelectionMainFrame =
   firstSelection.type === "FRAME" &&
   firstSelection.getRelaunchData()?.update === MAIN_FRAME_RELAUNCH_DATA.UPDATE;
 
-const mainFrame = isFirstSelectionMainFrame ? (firstSelection as FrameNode) : createMainFrame();
-if (isFirstSelectionMainFrame) for (const child of mainFrame.children) child.remove();
+const mainFrame = isFirstSelectionMainFrame
+  ? (firstSelection as FrameNode)
+  : createMainFrame();
+if (isFirstSelectionMainFrame)
+  for (const child of mainFrame.children) child.remove();
 
-const variableCollections = figma.variables.getLocalVariableCollections();
+figma.variables
+  .getLocalVariableCollectionsAsync()
+  .then((variableCollections) => {
+    if (variableCollections.length === 0) {
+      figma.notify("보여줄 수 있는 variable collection이 없어요.", {
+        error: true,
+      });
+      figma.closePlugin();
+    }
 
-if (variableCollections.length === 0) {
-  figma.notify("보여줄 수 있는 variable collection이 없어요.", { error: true });
-  figma.closePlugin();
-}
+    writeVariables({
+      mainFrame,
+      variableCollections,
+      combinationFrameNames: {
+        default: COMBINATION_INPUT_FRAME_NAMES.DEFAULT,
+        dark: COMBINATION_INPUT_FRAME_NAMES.DARK,
+      },
+      possibleSuffixes: SUFFIXES,
+    }).then(() => {
+      figma.currentPage.selection = [mainFrame];
+      figma.viewport.scrollAndZoomIntoView([mainFrame]);
 
-writeVariables({
-  mainFrame,
-  variableCollections,
-  combinationFrameNames: {
-    default: COMBINATION_INPUT_FRAME_NAMES.DEFAULT,
-    dark: COMBINATION_INPUT_FRAME_NAMES.DARK,
-  },
-  possibleSuffixes: SUFFIXES,
-}).then(() => {
-  figma.currentPage.selection = [mainFrame];
-  figma.viewport.scrollAndZoomIntoView([mainFrame]);
+      figma.notify("생성 완료!");
 
-  figma.notify("생성 완료!");
-
-  figma.closePlugin();
-});
+      figma.closePlugin();
+    });
+  });
