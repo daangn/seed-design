b47543136e3bc873a747cf3c770fd61bbb96673d
junghyeonsu <jung660317@naver.com>
2024-10-02 12:41:45 +0900

    chore: add comment

--- component-docs/schemas/component.ts
+++ component-docs/schemas/component.ts
@@ -1,12 +1,41 @@
 import { z } from "zod";
 
 export const componentMetadataSchema = z.object({
+  /**
+   * @description 컴포넌트 이름
+   * @example chip-tabs, alert-dialog
+   */
   name: z.string(),
+
   description: z.string().optional(),
+
+  /**
+   * @description 컴포넌트 의존성
+   * @example @seed-design/react-tabs
+   */
   dependencies: z.array(z.string()).optional(),
+
+  /**
+   * @description 컴포넌트 개발 의존성
+   */
   devDependencies: z.array(z.string()).optional(),
+
+  /**
+   * @description 컴포넌트 내부의 Seed Design 컴포넌트 의존성
+   * @example action-button
+   */
   innerDependencies: z.array(z.string()).optional(),
+
+  /**
+   * @description 컴포넌트 코드 스니펫 경로, 여러 파일이 될 수 있어서 배열로 정의
+   * @example component/alert-dialog.tsx
+   */
   snippets: z.array(z.string()),
+
+  /**
+   * @description 컴포넌트 타입
+   * pattern, component과 같은 타입들이 추가될 수 있음.
+   */
   type: z.enum(["component"]),
 });
 
