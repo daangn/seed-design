9efb944be3a087cc1c2ce52379426ce467d3869b
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-10-15 18:06:43 +0900

    feat: fix migrating names on importing default exports

---
+++ packages/cli/src/utils/migrate.ts
@@ -0,0 +1,118 @@
+import jscodeshift from "jscodeshift";
+
+export interface ImportTransformers {
+  source: { find: string; replace: string }[];
+  identifier: { find: RegExp; replace: string }[];
+}
+
+interface MigrateImportDeclarationsParams {
+  importDeclarations: jscodeshift.Collection<jscodeshift.ImportDeclaration>;
+  importTransformers: ImportTransformers;
+}
+
+export function migrateImportDeclarations({
+  importDeclarations,
+  importTransformers,
+}: MigrateImportDeclarationsParams) {
+  importDeclarations.replaceWith((imp) => {
+    const currentSourceValue = imp.node.source.value;
+    const currentSpecifiers = imp.node.specifiers;
+    const currentImportKind = imp.node.importKind;
+
+    const newSourceValue = (() => {
+      if (typeof currentSourceValue !== "string") return currentSourceValue;
+
+      const sourceReplaced = importTransformers.source.reduce(
+        (acc, { find, replace }) => acc.replace(find, replace),
+        currentSourceValue,
+      );
+
+      const slashSplits = sourceReplaced.split("/");
+
+      const itemReplaced = slashSplits
+        .map((split, index) => {
+          if (index !== slashSplits.length - 1) return split;
+
+          return importTransformers.identifier.reduce(
+            (acc, { find, replace }) => acc.replace(find, replace),
+            split,
+          );
+        })
+        .join("/");
+
+      return itemReplaced;
+    })();
+
+    console.log(`source: ${currentSourceValue} -> ${newSourceValue}`);
+
+    let impactedSpecifierCount = 0;
+
+    const newSpecifiers = currentSpecifiers.map((currentSpecifier) => {
+      switch (currentSpecifier.type) {
+        case "ImportSpecifier": {
+          // import { IconHeart } from "some-package";
+          const newImportedName = importTransformers.identifier.reduce(
+            (acc, { find, replace }) => acc.replace(find, replace),
+            currentSpecifier.imported.name,
+          );
+
+          const newImportedIdentifier = jscodeshift.identifier(newImportedName);
+
+          const hasNoChange = newImportedName === currentSpecifier.imported.name;
+
+          if (hasNoChange) return currentSpecifier;
+
+          impactedSpecifierCount++;
+
+          console.log(`identifier: ${currentSpecifier.imported.name} -> ${newImportedName}`);
+
+          // import { IconHeart as Heart } from "some-package"; 에서
+          // imported: "IconHeart", local: "Heart"
+          return jscodeshift.importSpecifier(newImportedIdentifier, currentSpecifier.local);
+        }
+        case "ImportDefaultSpecifier": {
+          // import Icon from "some-package";
+          return currentSpecifier;
+        }
+        case "ImportNamespaceSpecifier": {
+          // import * as Icon from "some-package";
+          return currentSpecifier;
+        }
+      }
+    });
+
+    const newImportDeclaration = jscodeshift.importDeclaration(
+      newSpecifiers,
+      jscodeshift.literal(newSourceValue),
+      currentImportKind,
+    );
+
+    console.log(`impacted specifiers: ${impactedSpecifierCount}`);
+    // console.log(matchedImportDeclarations.toSource());
+
+    return newImportDeclaration;
+  });
+}
+
+interface MigrateIdentifiersParams {
+  identifiers: jscodeshift.Collection<jscodeshift.Identifier>;
+  identifierTransformers: ImportTransformers["identifier"];
+}
+
+export function migrateIdentifiers({
+  identifiers,
+  identifierTransformers,
+}: MigrateIdentifiersParams) {
+  identifiers.replaceWith((identifier) => {
+    const currentName = identifier.node.name;
+
+    const newName = identifierTransformers.reduce(
+      (acc, { find, replace }) => acc.replace(find, replace),
+      currentName,
+    );
+
+    console.log(`identifier: ${currentName} -> ${newName}`);
+
+    return jscodeshift.identifier(newName);
+  });
+}
