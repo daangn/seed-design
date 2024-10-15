ed2d3263de6cb9174830cb1d25e6a177122ac90b
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-10-15 19:40:37 +0900

    chore: test better & replace regexes with strings

---
+++ packages/cli/src/test/icon-shift/cases/migrateImportDeclarationsWithMigrateIdentifiers/input.tsx
@@ -0,0 +1,13 @@
+// @ts-nocheck
+
+import { IconLike } from "some-package";
+
+export function IconDiv() {
+  console.log(IconLike);
+
+  return (
+    <div>
+      <IconLike />
+    </div>
+  );
+}
