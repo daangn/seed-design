90745fdb5edfa06f5356c22419b8038b2b3ae45f
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-10-16 16:22:41 +0900

    refactor(codemod): simplify tests (wip)

--- packages/codemod/icon-shift/icon-shift/test/cases/migrateImportDeclarations/input.tsx
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-
-import { IconLike } from "some-package";
--- packages/codemod/icon-shift/icon-shift/test/cases/migrateImportDeclarationsVariousTypes/expected.tsx
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-
-import { IconHeart as IconLike } from "some-new-package";
-import { IconStar as IconFavorite } from "some-new-package";
-import IconNight from "some-new-package/IconMoon";
-import * as IconHot from "some-new-package/IconFlame";
