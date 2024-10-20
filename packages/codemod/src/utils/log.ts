69f2e5a7e9d691830bc0f9162814bda260ce9042
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2024-10-20 14:38:09 +0900

    fix(codemod): remove unnecessary log files

--- packages/codemod/src/utils/log.ts
+++ packages/codemod/src/utils/log.ts
@@ -3,18 +3,10 @@ import { type LoggerOptions, format, transports } from "winston";
 export const loggerOptions: LoggerOptions = {
   level: "info",
   format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss", // Customize the timestamp format if needed
-    }),
-    format.printf(({ level, message, timestamp }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    }),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf(
+      ({ level, message, timestamp }) => `${timestamp} [${level.toUpperCase()}]: ${message}`,
+    ),
   ),
-  transports: [
-    new transports.File({ filename: "error.log", level: "error" }),
-    new transports.File({ filename: "combined.log", level: "debug" }),
-  ],
-  exitOnError: false,
-  handleExceptions: true,
-  exceptionHandlers: [new transports.File({ filename: "./exceptions.log" })],
+  transports: [new transports.File({ filename: "combined.log", level: "debug" })],
 };
