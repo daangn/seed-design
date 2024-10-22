42eda9973054aa827e7c30ad8df56e71782561b4
junghyeonsu <jung660317@naver.com>
2024-10-22 15:15:57 +0900

    chore(docs): remove vanilla extract

--- docs/registry/component/skeleton.tsx
+++ docs/registry/component/skeleton.tsx
@@ -2,8 +2,6 @@
 
 import * as React from "react";
 
-import * as css from "./skeleton.css";
-
 interface SkeletonProps {
   width: number | string;
   height: number | string;
@@ -13,18 +11,20 @@ interface SkeletonProps {
 
 // TODO: Spec
 // TODO: Recipe
-export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>((props, ref) => {
-  const { width, height, borderRadius, type = "wave" } = props;
-  return (
-    <div className={css.root}>
-      <div
-        ref={ref}
-        style={{
-          width,
-          height,
-        }}
-        className={css.skeleton({ type, borderRadius })}
-      />
-    </div>
-  );
-});
+export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+  (props, ref) => {
+    const { width, height, borderRadius, type = "wave" } = props;
+    return (
+      <div className="root">
+        <div
+          ref={ref}
+          style={{
+            width,
+            height,
+          }}
+          className={`skeleton ${borderRadius} ${type}`}
+        />
+      </div>
+    );
+  },
+);
