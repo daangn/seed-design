95b140fe52f86fc9f77abdd3d86c4a4888b834ac
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-03-14 01:56:22 +0900

    feat(codemod): support dynamic import in ESM (#592)
    * feat: cjs
    
    * chore: changeset
    
    * chore: change changeset message

--- packages/codemod/dev.mjs
+++ packages/codemod/dev.mjs
@@ -2,36 +2,37 @@ import esbuild from "esbuild";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
+/** @type {import('esbuild').BuildOptions} */
+const commonConfig = {
+  bundle: true,
+  write: true,
+  treeShaking: true,
+  sourcemap: false,
+  minify: false,
+  format: "cjs",
+  platform: "node",
+  target: ["node16"],
+  external: ["jscodeshift"],
+};
+
+// Build main entry
 esbuild
   .context({
+    ...commonConfig,
     entryPoints: ["./src/index.ts"],
-    outfile: "./bin/index.mjs",
-    bundle: true,
-    write: true,
-    treeShaking: true,
-    sourcemap: false,
-    minify: false,
-    format: "esm",
-    platform: "node",
-    target: ["esnext"],
+    outfile: "./bin/index.cjs",
   })
   .then((ctx) => ctx.watch())
   .catch(() => process.exit(1));
 
+// Build transforms
 fs.readdirSync("./src/transforms").forEach((folder) => {
   esbuild
     .context({
+      ...commonConfig,
       entryPoints: [path.join("./src/transforms", folder, "index.ts")],
       outdir: path.join("./bin/transforms", folder),
-      outExtension: { ".js": ".mjs" },
-      bundle: true,
-      write: true,
-      treeShaking: true,
-      sourcemap: false,
-      minify: false,
-      format: "esm",
-      platform: "node",
-      target: ["esnext"],
+      outExtension: { ".js": ".js" },
     })
     .then((ctx) => ctx.watch())
     .catch(() => process.exit(1));
