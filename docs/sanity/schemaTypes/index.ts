ebc6c67bbc9dd656f48b487d5b96818aafc5742f
malangcat <asher@daangn.com>
2025-03-19 19:36:44 +0900

    feat: sanity card type

--- docs/sanity/schemaTypes/index.ts
+++ docs/sanity/schemaTypes/index.ts
@@ -1,10 +1,10 @@
 import { type SchemaTypeDefinition } from "sanity";
 
 import blockContentType from "./blockContent";
-import guidelineType from "./guideline";
 import blogType from "./blog";
 import categoryType from "./category";
 import { doDontSectionType } from "./doDontType";
+import guidelineType from "./guideline";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [blockContentType, guidelineType, blogType, categoryType, doDontSectionType],
