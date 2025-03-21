17a23f35c08a6fbf3161a8f038da2116b8afa130
malangcat <asher@daangn.com>
2025-03-21 18:04:42 +0900

    refactor(docs): remove unused Stackflow components and configuration files

--- docs/components/stackflow/stackflow.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, ActivityDefinition, RegisteredActivityName } from "@stackflow/config";
-
-export const getConfig = (activities: ActivityDefinition<RegisteredActivityName>[]) =>
-  defineConfig({
-    activities,
-    transitionDuration: 270,
-    initialActivity: () => activities[0].name,
-  });
