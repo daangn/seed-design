5e35e192de6033875fffa27237943a2c30ed4120
malangcat <asher@daangn.com>
2025-03-24 12:58:28 +0900

    fix: update default Sanity API version to 2025-03-23

--- docs/sanity/env.ts
+++ docs/sanity/env.ts
@@ -1,4 +1,4 @@
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-12-06";
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-03-23";
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
