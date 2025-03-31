e7feb3b5892522b136fb5cb077e6ebfc4764ad8d
malangcat <asher@daangn.com>
2025-03-31 09:35:33 +0900

    feat: update dimension token

--- packages/qvism-preset/src/vars/dimension/spacing-y.mjs
+++ packages/qvism-preset/src/vars/dimension/spacing-y.mjs
@@ -1,2 +1,3 @@
 export const componentDefault = "var(--seed-dimension-spacing-y-component-default)";
 export const navToTitle = "var(--seed-dimension-spacing-y-nav-to-title)";
+export const screenBottom = "var(--seed-dimension-spacing-y-screen-bottom)";
