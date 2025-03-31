72f344f6b95bc073f7c38892ca93d3a53920dc01
Asher <102206520+malangcat@users.noreply.github.com>
2025-03-31 09:35:53 +0900

    feat: update dimension token (#632)

---
+++ .changeset/sour-planes-yell.md
@@ -0,0 +1,5 @@
+---
+"@seed-design/css": patch
+---
+
+`$dimension.spacing-y.screen-bottom` 토큰을 추가합니다.
--- packages/qvism-preset/src/vars/dimension/spacing-y.d.ts
+++ packages/qvism-preset/src/vars/dimension/spacing-y.d.ts
@@ -1,2 +1,3 @@
 export declare const componentDefault = "var(--seed-dimension-spacing-y-component-default)";
 export declare const navToTitle = "var(--seed-dimension-spacing-y-nav-to-title)";
+export declare const screenBottom = "var(--seed-dimension-spacing-y-screen-bottom)";
