d75011d31475f8ca5e4fef3a7bda99ca59aed7e6
malangcat <asher@daangn.com>
2025-04-05 20:23:43 +0900

    chore(figma): change fixture testing strategy

--- packages/figma/test/test.ts
+++ packages/figma/test/test.ts
@@ -3,11 +3,10 @@ import fs from "fs";
 import { generateCode } from "../src/codegen";
 import { createRestNormalizer } from "../src/normalizer/from-rest";
 
-const response = JSON.parse(
-  fs.readFileSync("./fixtures/fixture.json", "utf8"),
-) as GetFileNodesResponse;
+const node = JSON.parse(
+  fs.readFileSync("./fixtures/fixture2.json", "utf8"),
+) as GetFileNodesResponse["nodes"]["string"];
 
-const node = Object.values(response.nodes)[0]!;
 const normalizer = createRestNormalizer({
   styles: node.styles,
   components: node.components,
@@ -17,8 +16,6 @@ const normalizedNode = normalizer(node.document);
 const code = generateCode(normalizedNode);
 const simplifiedDesign = {
   name: node.document.name,
-  lastModified: response.lastModified,
-  thumbnailUrl: response.thumbnailUrl,
   code,
 };
 console.log(simplifiedDesign.code);
