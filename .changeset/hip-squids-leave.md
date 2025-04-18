d68dc8e00443205e441eebd3717e57b460847bb9
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-04-18 18:39:46 +0900

    feat: replace-seed-design-token-typography-classname

--- .changeset/hip-squids-leave.md
+++ .changeset/hip-squids-leave.md
@@ -4,3 +4,4 @@
 
 - replace-custom-seed-design-vars를 추가해요
 - replace-custom-seed-design-typography를 추가해요
+- replace-seed-design-token-typography-classname VE에서 잘 변경되지 않던 이슈를 해결해요
--- packages/codemod/src/transforms/replace-seed-design-token-typography-classname/__testfixtures__/basic.output.tsx
+++ packages/codemod/src/transforms/replace-seed-design-token-typography-classname/__testfixtures__/basic.output.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 
 import { text } from "@seed-design/css/recipes/text";
+import { style } from "@vanilla-extract/css";
 
 const typography = {
   one: text({ textStyle: "t7Regular" }),
@@ -8,3 +9,13 @@ const typography = {
   three: text({ textStyle: "t1Regular" }),
   four: text({ textStyle: "t1Regular" }),
 }
+
+export const footer = style([
+  text({ textStyle: "t3Regular" }),
+  {
+    padding: "16px",
+    marginTop: "12px",
+    color: vars.$scale.color.gray700,
+    backgroundColor: vars.$semantic.color.paperContents,
+  },
+]);
