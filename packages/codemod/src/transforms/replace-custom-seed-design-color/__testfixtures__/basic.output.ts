438dd68a41e198342735d7befecf47125b86e4e6
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-04-25 18:04:21 +0900

    Revert "feat: replace-custom-seed-design-color 구현"
    This reverts commit 05806371b88b5fe9b459737e86c02cc2746437a5.

--- packages/codemod/src/transforms/replace-custom-seed-design-color/__testfixtures__/basic.output.ts
+++ packages/codemod/src/transforms/replace-custom-seed-design-color/__testfixtures__/basic.output.ts
@@ -1,29 +1,29 @@
-import { vars } from "@seed-design/css/vars";
 // @ts-nocheck
 import { style } from "@vanilla-extract/css";
+import { color, background } from "@/shared/styles";
 
 export const container = style({
-  backgroundColor: vars.$color.palette.gray200,
-  color: vars.$color.palette.gray1000,
-  borderColor: vars.$color.palette.carrot600,
+  backgroundColor: color.palette.gray200,
+  color: color.palette.gray1000,
+  borderColor: color.palette.carrot600,
 });
 
 export const box = style({
-  background: vars.$color.palette.gray100,
-  color: vars.$color.palette.red700,
-  border: `1px solid ${vars.$color.palette.gray400}`,
+  background: color.palette.gray100,
+  color: color.palette.red700,
+  border: `1px solid ${color.palette.gray400}`,
 });
 
 export const alert = style({
-  backgroundColor: vars.$color.palette.red100,
-  color: vars.$color.palette.red900,
+  backgroundColor: color.palette.red100,
+  color: color.palette.red900,
 });
 
 export const button = style({
-  backgroundColor: vars.$color.palette.gray200,
-  color: vars.$color.palette.gray1000,
+  backgroundColor: background.palette.gray200,
+  color: color.palette.gray1000,
 });
 
 export const highlight = style({
-  color: vars.$color.palette.carrot600,
+  color: color.palette.carrot600,
 }); 
