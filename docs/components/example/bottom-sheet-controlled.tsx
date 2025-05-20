42a57a48372aa5e8fdeb68fc98e04ca7eb4f0667
malangcat <asher@daangn.com>
2025-05-20 17:25:32 +0900

    docs: add examples for BottomSheet

---
+++ docs/components/example/bottom-sheet-controlled.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { ActionButton } from "seed-design/ui/action-button";
+import {
+  BottomSheetBody,
+  BottomSheetContent,
+  BottomSheetFooter,
+  BottomSheetRoot,
+} from "seed-design/ui/bottom-sheet";
+
+const BottomSheetControlled = () => {
+  const [open, setOpen] = useState(false);
+
+  const scheduleOpen = () => {
+    setTimeout(() => {
+      setOpen(true);
+    }, 1000);
+  };
+
+  return (
+    <>
+      <ActionButton onClick={scheduleOpen}>1초 후 열기</ActionButton>
+      <BottomSheetRoot open={open} onOpenChange={setOpen}>
+        <BottomSheetContent title="제목" description="설명을 작성할 수 있어요">
+          {/* If you need to omit padding, pass px={0}. */}
+          <BottomSheetBody minHeight="x16">Content</BottomSheetBody>
+          <BottomSheetFooter>
+            <ActionButton variant="neutralSolid">확인</ActionButton>
+          </BottomSheetFooter>
+        </BottomSheetContent>
+      </BottomSheetRoot>
+    </>
+  );
+};
+
+export default BottomSheetControlled;
---
+++ docs/components/example/bottom-sheet-trigger.tsx
@@ -0,0 +1,26 @@
+import { ActionButton } from "seed-design/ui/action-button";
+import {
+  BottomSheetBody,
+  BottomSheetContent,
+  BottomSheetFooter,
+  BottomSheetRoot,
+  BottomSheetTrigger,
+} from "seed-design/ui/bottom-sheet";
+
+const BottomSheetTriggerExample = () => {
+  return (
+    <BottomSheetRoot>
+      <BottomSheetTrigger asChild>
+        <ActionButton>Open</ActionButton>
+      </BottomSheetTrigger>
+      <BottomSheetContent title="제목">
+        <BottomSheetBody minHeight="x16">Content</BottomSheetBody>
+        <BottomSheetFooter>
+          <ActionButton variant="neutralSolid">확인</ActionButton>
+        </BottomSheetFooter>
+      </BottomSheetContent>
+    </BottomSheetRoot>
+  );
+};
+
+export default BottomSheetTriggerExample;
