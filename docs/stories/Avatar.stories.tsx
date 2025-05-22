410deb1c4c0a021fca17de842571aa46d8f8e340
malangcat <asher@daangn.com>
2025-05-22 21:56:17 +0900

    fix(docs): update clip-path attributes to clipPath in SVG components

--- docs/stories/Avatar.stories.tsx
+++ docs/stories/Avatar.stories.tsx
@@ -36,7 +36,7 @@ const FLOWER_BADGE_SVG = (
 
 const SHIELD_BADGE_SVG = (
   <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_23077_49875)">
+    <g clipPath="url(#clip0_23077_49875)">
       <path
         fill-rule="evenodd"
         clip-rule="evenodd"
