acce9e34cfb0f602305d7ac97af46cc8324bfac0
malangcat <asher@daangn.com>
2025-06-02 16:56:13 +0900

    docs: update contextual floating button examples

--- docs/components/example/contextual-floating-button-layer.tsx
+++ docs/components/example/contextual-floating-button-layer.tsx
@@ -1,5 +1,12 @@
+import { IconPlusLine } from "@karrotmarket/react-monochrome-icon";
+import { PrefixIcon } from "@seed-design/react";
 import { ContextualFloatingButton } from "seed-design/ui/contextual-floating-button";
 
 export default function ContextualFloatingButtonLayer() {
-  return <ContextualFloatingButton variant="layer">Layer Variant</ContextualFloatingButton>;
+  return (
+    <ContextualFloatingButton variant="layer">
+      <PrefixIcon svg={<IconPlusLine />} />
+      Layer Variant
+    </ContextualFloatingButton>
+  );
 }
--- docs/components/example/contextual-floating-button-loading.tsx
+++ docs/components/example/contextual-floating-button-loading.tsx
@@ -1,3 +1,5 @@
+import { IconPlusLine } from "@karrotmarket/react-monochrome-icon";
+import { PrefixIcon } from "@seed-design/react";
 import { useState } from "react";
 import { ContextualFloatingButton } from "seed-design/ui/contextual-floating-button";
 
@@ -12,6 +14,7 @@ export default function ContextualFloatingButtonLoading() {
   // 이벤트 핸들링이 필요할 수 있으므로 loading은 disabled를 포함하지 않습니다. 이벤트 발생을 원하지 않는 경우, disabled 속성을 추가해주세요.
   return (
     <ContextualFloatingButton loading={loading} onClick={handleClick}>
+      <PrefixIcon svg={<IconPlusLine />} />
       시간이 걸리는 액션
     </ContextualFloatingButton>
   );
