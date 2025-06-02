1d6a99ada5000791f9733ae477f0a61947a1d49f
malangcat <asher@daangn.com>
2025-06-02 16:56:13 +0900

    docs: update contextual floating button examples

--- docs/components/example/contextual-floating-button-solid.tsx
+++ docs/components/example/contextual-floating-button-solid.tsx
@@ -1,5 +1,12 @@
+import { IconPlusLine } from "@karrotmarket/react-monochrome-icon";
+import { PrefixIcon } from "@seed-design/react";
 import { ContextualFloatingButton } from "seed-design/ui/contextual-floating-button";
 
 export default function ContextualFloatingButtonSolid() {
-  return <ContextualFloatingButton variant="solid">Solid Variant</ContextualFloatingButton>;
+  return (
+    <ContextualFloatingButton variant="solid">
+      <PrefixIcon svg={<IconPlusLine />} />
+      Solid Variant
+    </ContextualFloatingButton>
+  );
 }
