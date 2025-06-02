37a4e2a2c799007b58e23a1e20d68f54a88c0cd0
malangcat <asher@daangn.com>
2025-06-02 19:51:06 +0900

    docs: improve TabsCarousel examples

---
+++ docs/components/example/tabs-transition.tsx
@@ -0,0 +1,42 @@
+import { TabsRoot, TabsList, TabsTrigger, TabsCarousel, TabsContent } from "seed-design/ui/tabs";
+
+export default function TabsTransition() {
+  return (
+    <div style={{ width: "360px" }}>
+      <TabsRoot defaultValue="2">
+        <TabsList>
+          <TabsTrigger value="1">라벨1</TabsTrigger>
+          <TabsTrigger value="2">라벨2</TabsTrigger>
+          <TabsTrigger value="3">라벨3</TabsTrigger>
+        </TabsList>
+        <TabsCarousel>
+          <TabsContent value="1">
+            <Content>Content 1</Content>
+          </TabsContent>
+          <TabsContent value="2">
+            <Content>Content 2</Content>
+          </TabsContent>
+          <TabsContent value="3">
+            <Content>Content 3</Content>
+          </TabsContent>
+        </TabsCarousel>
+      </TabsRoot>
+    </div>
+  );
+}
+
+const Content = (props: React.PropsWithChildren) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "300px",
+        backgroundColor: "var(--seed-color-bg-layer-default)",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
