e9db89fd7e503b52460060c2dc64d77d63494e57
malangcat <asher@daangn.com>
2025-06-10 15:58:13 +0900

    feat(react): add `asChild` prop to layout components

---
+++ .changeset/kind-drinks-reply.md
@@ -0,0 +1,5 @@
+---
+"@seed-design/react": patch
+---
+
+레이아웃 컴포넌트에 `asChild` 속성을 추가합니다.
--- packages/react/src/components/Box/Box.tsx
+++ packages/react/src/components/Box/Box.tsx
@@ -1,14 +1,23 @@
 import clsx from "clsx";
 import * as React from "react";
 import { useStyleProps, type StyleProps } from "../../utils/styled";
+import { Slot } from "@radix-ui/react-slot";
 
 export interface BoxProps extends StyleProps, Omit<React.HTMLAttributes<HTMLDivElement>, "color"> {
   as?: React.ElementType;
+
+  asChild?: boolean;
 }
 
 export const Box = React.forwardRef<HTMLDivElement, BoxProps>((props, ref) => {
   const { style, restProps } = useStyleProps(props);
-  const { as: Comp = "div", className, ...nativeProps } = restProps;
+  const { as: Comp = "div", asChild = false, className, ...nativeProps } = restProps;
+
+  if (asChild) {
+    return (
+      <Slot ref={ref} className={clsx("seed-box", className)} style={style} {...nativeProps} />
+    );
+  }
 
   return <Comp ref={ref} className={clsx("seed-box", className)} style={style} {...nativeProps} />;
 });
