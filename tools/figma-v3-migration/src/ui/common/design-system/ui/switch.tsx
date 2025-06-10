69035e9e9cadc6d1e7bab5052913a89fc697b6e5
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-06-10 12:09:24 +0900

    feat(figma-v3-migration): add switch

---
+++ tools/figma-v3-migration/src/ui/common/design-system/ui/switch.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Switch as SeedSwitch } from "@seed-design/react";
+
+export interface SwitchProps extends SeedSwitch.RootProps {
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+
+  rootRef?: React.Ref<HTMLLabelElement>;
+
+  /**
+   * label is supported in small size only currently.
+   */
+  label?: React.ReactNode;
+}
+
+/**
+ * @see https://seed-design.io/react/components/switch
+ */
+export const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
+  ({ inputProps, rootRef, label, ...otherProps }, ref) => {
+    return (
+      <SeedSwitch.Root ref={rootRef} {...otherProps}>
+        <SeedSwitch.Control>
+          <SeedSwitch.Thumb />
+        </SeedSwitch.Control>
+        {label && <SeedSwitch.Label>{label}</SeedSwitch.Label>}
+        <SeedSwitch.HiddenInput ref={ref} {...inputProps} />
+      </SeedSwitch.Root>
+    );
+  },
+);
+Switch.displayName = "Switch";
