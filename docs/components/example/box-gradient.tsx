b0f7a4ec331ce44fb4e5d775eebe9ff4a24bea20
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-07-01 11:23:56 +0900

    feat: gradient direction to css property (#785)
    * feat: change gradient property to css prop
    
    * chore: changeset
    
    * docs(docs): box gradient example

--- docs/components/example/box-gradient.tsx
+++ docs/components/example/box-gradient.tsx
@@ -1,16 +1,28 @@
-import { Box } from "@seed-design/react";
+import { Box, VStack } from "@seed-design/react";
 
 export default function BoxGradient() {
   return (
-    <Box
-      backgroundGradient="highlightMagic"
-      backgroundGradientDirection="43deg"
-      borderRadius="r2"
-      color="palette.staticWhite"
-      px="x3"
-      py="x2"
-    >
-      Box Gradient
-    </Box>
+    <VStack gap="x2">
+      <Box
+        backgroundGradient="highlightMagic"
+        backgroundGradientDirection="43deg"
+        borderRadius="r2"
+        color="palette.staticWhite"
+        px="x3"
+        py="x2"
+      >
+        Box Gradient
+      </Box>
+      <Box
+        backgroundGradient="highlightMagic"
+        backgroundGradientDirection="to bottom"
+        borderRadius="r2"
+        color="palette.staticWhite"
+        px="x3"
+        py="x2"
+      >
+        Box Gradient
+      </Box>
+    </VStack>
   );
 }
