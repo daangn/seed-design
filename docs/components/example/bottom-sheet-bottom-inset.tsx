69ccbb6c3261e6e8332744d702ccf5dd3e2a2e20
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-07-03 11:39:34 +0900

    docs: add bottom-sheet examples

---
+++ docs/components/example/bottom-sheet-bottom-inset.tsx
@@ -0,0 +1,32 @@
+import { ActionButton } from "seed-design/ui/action-button";
+import {
+  BottomSheetBody,
+  BottomSheetContent,
+  BottomSheetFooter,
+  BottomSheetRoot,
+  BottomSheetTrigger,
+} from "seed-design/ui/bottom-sheet";
+
+const BottomSheetBottomInset = () => {
+  return (
+    <BottomSheetRoot>
+      <BottomSheetTrigger asChild>
+        <ActionButton>Open</ActionButton>
+      </BottomSheetTrigger>
+      <BottomSheetContent
+        title="제목"
+        description="설명을 작성할 수 있어요"
+        // 모바일 브라우저에서 아래 여백을 주기 위해 사용
+        style={{ paddingBottom: "var(--seed-safe-area-bottom)" }}
+      >
+        {/* If you need to omit padding, pass px={0}. */}
+        <BottomSheetBody minHeight="x16">Content</BottomSheetBody>
+        <BottomSheetFooter>
+          <ActionButton variant="neutralSolid">확인</ActionButton>
+        </BottomSheetFooter>
+      </BottomSheetContent>
+    </BottomSheetRoot>
+  );
+};
+
+export default BottomSheetBottomInset;
---
+++ docs/components/example/bottom-sheet-max-height.tsx
@@ -0,0 +1,37 @@
+import { Box, VStack } from "@seed-design/react";
+import { ActionButton } from "seed-design/ui/action-button";
+import {
+  BottomSheetBody,
+  BottomSheetContent,
+  BottomSheetFooter,
+  BottomSheetRoot,
+  BottomSheetTrigger,
+} from "seed-design/ui/bottom-sheet";
+
+const BottomSheetMaxHeight = () => {
+  return (
+    <BottomSheetRoot>
+      <BottomSheetTrigger asChild>
+        <ActionButton>Open</ActionButton>
+      </BottomSheetTrigger>
+      <BottomSheetContent title="제목" description="설명을 작성할 수 있어요">
+        {/* If you need to omit padding, pass px={0}. */}
+        <BottomSheetBody>
+          <VStack maxHeight="300px" overflowY="auto">
+            <VStack justifyContent="center" alignItems="center" gap="x4" height="100%">
+              <Box width="100%" height="100px" bg="bg.layerBasement" />
+              <Box width="100%" height="100px" bg="bg.layerBasement" />
+              <Box width="100%" height="100px" bg="bg.layerBasement" />
+              <Box width="100%" height="100px" bg="bg.layerBasement" />
+            </VStack>
+          </VStack>
+        </BottomSheetBody>
+        <BottomSheetFooter>
+          <ActionButton variant="neutralSolid">확인</ActionButton>
+        </BottomSheetFooter>
+      </BottomSheetContent>
+    </BottomSheetRoot>
+  );
+};
+
+export default BottomSheetMaxHeight;
