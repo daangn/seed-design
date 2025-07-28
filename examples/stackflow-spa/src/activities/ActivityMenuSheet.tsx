8b19bad5086f527213c1e415af2c198e0cfd7c64
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-07-28 17:45:49 +0900

    feat: update example activities

---
+++ examples/stackflow-spa/src/activities/ActivityMenuSheet.tsx
@@ -0,0 +1,68 @@
+import { useActivity, type ActivityComponentType } from "@stackflow/react";
+
+import {
+  IconPencilLine,
+  IconPlusLine,
+  IconTrashcanLine,
+} from "@karrotmarket/react-monochrome-icon";
+import {
+  MenuSheetContent,
+  MenuSheetGroup,
+  MenuSheetItem,
+  MenuSheetRoot,
+} from "../seed-design/ui/menu-sheet";
+import { createCallbackActivity } from "../stackflow/createCallbackActivity";
+import { PrefixIcon } from "@seed-design/react";
+
+type Action = "add" | "edit" | "delete" | "test1" | "test2";
+
+export const menuSheetCallback = createCallbackActivity(
+  "ActivityMenuSheet",
+  {} as {
+    action: Action;
+  },
+);
+
+const ActivityMenuSheet: ActivityComponentType = () => {
+  const { pop } = menuSheetCallback.useCallbackPop();
+  const activity = useActivity();
+
+  const handleAction = (action: Action) => () => {
+    pop({ action });
+  };
+
+  const handleClose = (open: boolean) => {
+    if (!open) {
+      pop();
+    }
+  };
+
+  console.log({ menuSheet: activity.isActive });
+
+  return (
+    <MenuSheetRoot open={activity.isActive} onOpenChange={handleClose}>
+      <MenuSheetContent title="Actions" layerIndex={activity.zIndex * 5}>
+        <MenuSheetGroup>
+          <MenuSheetItem onClick={handleAction("add")}>
+            <PrefixIcon svg={<IconPlusLine />} />
+            Add
+          </MenuSheetItem>
+          <MenuSheetItem onClick={handleAction("edit")}>
+            <PrefixIcon svg={<IconPencilLine />} />
+            Edit
+          </MenuSheetItem>
+        </MenuSheetGroup>
+        <MenuSheetGroup labelAlign="center">
+          <MenuSheetItem onClick={handleAction("test1")}>Test1</MenuSheetItem>
+          <MenuSheetItem onClick={handleAction("test2")}>Test2</MenuSheetItem>
+          <MenuSheetItem onClick={handleAction("delete")} tone="critical" labelAlign="left">
+            <PrefixIcon svg={<IconTrashcanLine />} />
+            Delete
+          </MenuSheetItem>
+        </MenuSheetGroup>
+      </MenuSheetContent>
+    </MenuSheetRoot>
+  );
+};
+
+export default ActivityMenuSheet;
