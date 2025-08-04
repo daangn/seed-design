7b13dd518bcc69ac68dbc1311189102615fd5429
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-04 14:25:11 +0900

    refactor: migrate existing configs

--- packages/figma/figma-extractor.config.ts
+++ packages/figma/figma-extractor.config.ts
@@ -1,39 +1,47 @@
-import type {
-  Config,
-  GenerateComponentMetadataOptions,
-  GenerateComponentSetMetadataOptions,
-} from "@seed-design/figma-extractor";
+import { createConfig, createPipeline, sources, writers } from "@seed-design/figma-extractor";
 
-const transform: GenerateComponentMetadataOptions["transform"] &
-  GenerateComponentSetMetadataOptions["transform"] = ({
-  name,
-  key,
-  componentPropertyDefinitions,
-}) => ({
-  name,
-  key,
-  ...(componentPropertyDefinitions && {
-    componentPropertyDefinitions: Object.fromEntries(
-      Object.entries(componentPropertyDefinitions).map(([key, { defaultValue, ...rest }]) => [
+const config = createConfig({
+  pipelines: {
+    components: createPipeline()
+      .source(sources.components)
+      .filter(
+        ({ name, componentSetId }) =>
+          (name.startsWith("🔵 ") || name.startsWith("🟢 ")) && componentSetId === undefined,
+      )
+      .sort((a, b) => a.name.localeCompare(b.name))
+      // drop defaultValue and take the rest
+      .transform(({ name, key, componentPropertyDefinitions }) => ({
+        name,
         key,
-        rest,
-      ]),
-    ),
-  }),
-});
-
-const config: Config = {
-  data: {
-    components: {
-      filter: ({ name, componentSetId }) =>
-        (name.startsWith("🔵 ") || name.startsWith("🟢 ")) && componentSetId === undefined,
-      transform,
-    },
-    componentSets: {
-      filter: ({ name }) => name.startsWith("🔵 ") || name.startsWith("🟢 "),
-      transform,
-    },
+        ...(componentPropertyDefinitions && {
+          componentPropertyDefinitions: Object.fromEntries(
+            Object.entries(componentPropertyDefinitions).map(([key, { defaultValue, ...rest }]) => [
+              key,
+              rest,
+            ]),
+          ),
+        }),
+      }))
+      .write(writers.default),
+    "component-sets": createPipeline()
+      .source(sources.componentSets)
+      .filter(({ name }) => name.startsWith("🔵 ") || name.startsWith("🟢 "))
+      .sort((a, b) => a.name.localeCompare(b.name))
+      // drop defaultValue and take the rest
+      .transform(({ name, key, componentPropertyDefinitions }) => ({
+        name,
+        key,
+        ...(componentPropertyDefinitions && {
+          componentPropertyDefinitions: Object.fromEntries(
+            Object.entries(componentPropertyDefinitions).map(([key, { defaultValue, ...rest }]) => [
+              key,
+              rest,
+            ]),
+          ),
+        }),
+      }))
+      .write(writers.default),
   },
-};
+});
 
 export default config;
--- tools/figma-v3-migration/figma-extractor.config.ts
+++ tools/figma-v3-migration/figma-extractor.config.ts
@@ -1,29 +1,23 @@
-import type { Config, GenerateComponentSetMetadataOptions } from "@seed-design/figma-extractor";
+import { createConfig, createPipeline, sources, writers } from "@seed-design/figma-extractor";
 
-const componentSetTransform: GenerateComponentSetMetadataOptions["transform"] = ({
-  name,
-  key,
-  componentPropertyDefinitions,
-}) => ({
-  name,
-  key,
-  ...(componentPropertyDefinitions && {
-    componentPropertyDefinitions: Object.fromEntries(
-      Object.entries(componentPropertyDefinitions).map(([key, { ...rest }]) => [key, rest]),
-    ),
-  }),
-});
-
-const config: Config = {
-  data: {
-    componentSets: {
-      transform: componentSetTransform,
-    },
-    styles: {
-      filter: ({ style_type }) => style_type === "TEXT",
-      transform: ({ name, key }) => ({ name, key }),
-    },
+const config = createConfig({
+  pipelines: {
+    styles: createPipeline()
+      .source(sources.styles)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .filter(({ style_type }) => style_type === "TEXT")
+      .transform(({ name, key }) => ({ name, key }))
+      .write(writers.default),
+    "component-sets": createPipeline()
+      .source(sources.componentSets)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .transform(({ name, key, componentPropertyDefinitions }) => ({
+        name,
+        key,
+        componentPropertyDefinitions,
+      }))
+      .write(writers.default),
   },
-};
+});
 
 export default config;
