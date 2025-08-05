29c55bad1d6d6f1345a6cb514ec0a56c5858f1f7
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-05 18:50:24 +0900

    refactor: refactor page filter

--- docs/app/react/_llms/page-filter.ts
+++ docs/app/react/_llms/page-filter.ts
@@ -3,10 +3,11 @@ import type { Page } from "fumadocs-core/source";
 export function shouldGenerateLLMFriendlyText({ slugs }: Page) {
   const [firstSlug, secondSlug] = slugs;
 
-  // include components/** but exclude components/concepts
-  if (firstSlug !== "components" || (firstSlug === "components" && secondSlug === "concepts")) {
-    return false;
-  }
+  // include components/**
+  if (firstSlug !== "components") return false;
+
+  // exclude components/concepts
+  if (secondSlug === "concepts") return false;
 
   return true;
 }
