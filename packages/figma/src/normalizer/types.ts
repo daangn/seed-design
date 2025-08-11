e1df7f0b7b608a4a8a47d991d96497e99f9c96a3
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-11 11:02:45 +0900

    feat: normalize overrides from instance layers

--- packages/figma/src/normalizer/types.ts
+++ packages/figma/src/normalizer/types.ts
@@ -121,6 +121,8 @@ export interface NormalizedInstanceNode extends NormalizedFrameTrait {
 
   componentSetKey?: string;
 
+  overrides?: FigmaRestSpec.InstanceNode["overrides"];
+
   children: NormalizedSceneNode[];
 }
 
