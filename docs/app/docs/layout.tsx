4c13e6e8c29df1caed026ba15445e0670e95b391
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-13 16:12:45 +0900

    docs: set default search scope (#870)
    * feat: set default search scope
    
    * refactor: refactor
    
    * refactor: refactor

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -2,13 +2,26 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { docsOptions } from "../layout.config";
 import { source } from "../source";
+import { RootProvider } from "fumadocs-ui/provider";
+import DefaultSearchDialog from "@/components/search/search";
+import { TAGS } from "@/app/api/search/constants";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const transformedTree = await source.getTransformedPageTree();
 
   return (
-    <DocsLayout {...docsOptions} tree={transformedTree}>
-      {children}
-    </DocsLayout>
+    <RootProvider
+      search={{
+        SearchDialog: DefaultSearchDialog,
+        options: {
+          defaultTag: TAGS.design.value,
+          tags: Object.values(TAGS),
+        },
+      }}
+    >
+      <DocsLayout {...docsOptions} tree={transformedTree}>
+        {children}
+      </DocsLayout>
+    </RootProvider>
   );
 }
