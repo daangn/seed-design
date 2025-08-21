6c2d99bca9e7a15562b6b0f64cf421ad415f5b1c
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-21 17:55:40 +0900

    feat: sync colors (#877)
    * feat: sync colors (no actual changes, just orders have been changed)
    
    * feat: generate

--- packages/css/vars/gradient.d.ts
+++ packages/css/vars/gradient.d.ts
@@ -1,8 +1,8 @@
 export declare const fadeLayerFloating = "var(--seed-gradient-fade-layer-floating)";
 export declare const fadeLayerDefault = "var(--seed-gradient-fade-layer-default)";
 export declare const glowMagic = "var(--seed-gradient-glow-magic)";
-export declare const shimmerMagic = "var(--seed-gradient-shimmer-magic)";
 export declare const glowMagicPressed = "var(--seed-gradient-glow-magic-pressed)";
 export declare const shimmerNeutral = "var(--seed-gradient-shimmer-neutral)";
-export declare const highlightMagic = "var(--seed-gradient-highlight-magic)";
+export declare const shimmerMagic = "var(--seed-gradient-shimmer-magic)";
 export declare const highlightMagicPressed = "var(--seed-gradient-highlight-magic-pressed)";
+export declare const highlightMagic = "var(--seed-gradient-highlight-magic)";
