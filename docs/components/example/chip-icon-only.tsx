fbf20e6703b58b6ab9018faabf89d006afe9be05
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-08-29 17:49:42 +0900

    docs: docs

--- docs/components/example/chip-icon-only.tsx
+++ docs/components/example/chip-icon-only.tsx
@@ -17,19 +17,24 @@ export default function ChipIconOnly() {
   return (
     <VStack gap="x3" align="center">
       <HStack gap="x2">
-        <Chip.Button layout="iconOnly">
+        <Chip.Button layout="iconOnly" aria-label="Refresh">
           <Icon svg={<IconArrowClockwiseCircularLine />} />
         </Chip.Button>
-        <Chip.Toggle layout="iconOnly" checked={checked} onCheckedChange={setChecked}>
+        <Chip.Toggle
+          layout="iconOnly"
+          checked={checked}
+          onCheckedChange={setChecked}
+          inputProps={{ "aria-label": "Receive notifications" }}
+        >
           <Icon svg={checked ? <IconBellLine /> : <IconBellSlashLine />} />
         </Chip.Toggle>
       </HStack>
       <Chip.RadioRoot defaultValue="3" aria-label="Timer">
         <HStack gap="x2">
-          <Chip.RadioItem value="3" layout="iconOnly" aria-label="3 seconds">
+          <Chip.RadioItem value="3" layout="iconOnly" inputProps={{ "aria-label": "3 seconds" }}>
             <Icon svg={<IconTimer_3Line />} />
           </Chip.RadioItem>
-          <Chip.RadioItem value="10" layout="iconOnly" aria-label="10 seconds">
+          <Chip.RadioItem value="10" layout="iconOnly" inputProps={{ "aria-label": "10 seconds" }}>
             <Icon svg={<IconTimer_10Line />} />
           </Chip.RadioItem>
         </HStack>
