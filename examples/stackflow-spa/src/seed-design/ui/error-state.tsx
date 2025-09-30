5f66fe360f3a0c0ada2e8cb833d9dd9c78667bcb
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-09-30 15:23:29 +0900

    chore: generate

--- examples/stackflow-spa/src/seed-design/ui/error-state.tsx
+++ examples/stackflow-spa/src/seed-design/ui/error-state.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Text, VStack, type TextProps } from "@seed-design/react";
 import * as React from "react";
 import { ActionButton, type ActionButtonProps } from "./action-button";
