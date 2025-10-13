44df24a3872a761c932d149c6761f47456c07f3c
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-10-13 11:45:39 +0900

    feat: log codegen events

--- tools/figma-codegen/build-figma-plugin.main.js
+++ tools/figma-codegen/build-figma-plugin.main.js
@@ -1,4 +1,16 @@
+require("dotenv").config();
+
+if (!process.env.POSTHOG_API_KEY || !process.env.POSTHOG_HOST) {
+  console.error("Please set POSTHOG_API_KEY and POSTHOG_HOST in .env file");
+  process.exit(1);
+}
+
 module.exports = (buildOptions) => ({
   ...buildOptions,
   resolveExtensions: [".ts", ".js", ".json", ".mjs"],
+  define: {
+    "Env.MODE": process.env.IS_DEV === "true" ? '"dev"' : '"prod"',
+    "Env.POSTHOG_API_KEY": `"${process.env.POSTHOG_API_KEY}"`,
+    "Env.POSTHOG_HOST": `"${process.env.POSTHOG_HOST}"`,
+  },
 });
