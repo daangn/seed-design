b24fc7c5c233fe8fecfb30c6372eb3ab1fb7805a
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-10-21 19:57:40 +0900

    refactor: export AppBarIconButtonProps from snippet app-bar

--- docs/registry/ui/app-bar.tsx
+++ docs/registry/ui/app-bar.tsx
@@ -2,7 +2,7 @@
 
 import { IconChevronLeftLine, IconXmarkLine } from "@karrotmarket/react-monochrome-icon"; // "@daangn/react-monochrome-icon"과 동일합니다.
 import { VStack } from "@seed-design/react";
-import { AppBar as SeedAppBar, type AppBarIconButtonProps } from "@seed-design/stackflow";
+import { AppBar as SeedAppBar } from "@seed-design/stackflow";
 import { useActions, useActivity } from "@stackflow/react";
 import * as React from "react";
 import { forwardRef } from "react";
@@ -54,6 +54,8 @@ export const AppBarMain = forwardRef<HTMLDivElement, AppBarMainProps>(
   },
 );
 AppBarMain.displayName = "AppBarMain";
+
+export interface AppBarIconButtonProps extends SeedAppBar.IconButtonProps {}
 
 export const AppBarIconButton = SeedAppBar.IconButton;
 
