7d6865f6abce623adb3b6ffe77bb0a4f108bede8
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-10-27 15:46:39 +0900

    docs

--- docs/examples/react/article/preview.tsx
+++ docs/examples/react/article/preview.tsx
@@ -8,7 +8,7 @@ export default function ArticlePreview() {
           Article은 일관된 selection 및 줄바꿈 정책을 사용할 수 있게 돕는 유틸리티 컴포넌트입니다.
         </Text>
         <Text as="p" textStyle="articleBody">
-          여기를 드래그해서 스타일을 확인해보세요.
+          여기를 드래그해서 선택해보세요.
         </Text>
       </Article>
     </VStack>
