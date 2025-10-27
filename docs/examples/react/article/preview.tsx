4bfa69803ed9d31fde510f180f8a24e5445cf6f2
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-10-27 15:36:51 +0900

    docs

--- docs/examples/react/article/preview.tsx
+++ docs/examples/react/article/preview.tsx
@@ -5,8 +5,7 @@ export default function ArticlePreview() {
     <VStack asChild gap="x2" width="400px">
       <Article>
         <Text as="p" textStyle="articleBody">
-          Article은 일관된 selection 스타일 및 줄바꿈 정책을 사용할 수 있게 돕는 유틸리티
-          컴포넌트입니다.
+          Article은 일관된 selection 및 줄바꿈 정책을 사용할 수 있게 돕는 유틸리티 컴포넌트입니다.
         </Text>
         <Text as="p" textStyle="articleBody">
           여기를 드래그해서 스타일을 확인해보세요.
