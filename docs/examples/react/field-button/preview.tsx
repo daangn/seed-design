4e9e90a559e144fc96ed2637cf7711ffd60be415
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-11-03 21:17:07 +0900

    docs

--- docs/examples/react/field-button/preview.tsx
+++ docs/examples/react/field-button/preview.tsx
@@ -3,7 +3,6 @@ import { FieldButton } from "seed-design/ui/field-button";
 export default function FieldButtonPreview() {
   return (
     <FieldButton
-      weight="bold"
       label="레이블"
       description="버튼에 대한 설명을 작성해주세요"
       buttonProps={{
