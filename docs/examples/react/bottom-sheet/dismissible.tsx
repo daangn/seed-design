8066c664f37d5914ee0c65a62a43717dfd18444c
정현수 <hyeonsu.github@gmail.com>
2025-11-04 16:11:54 +0900

    docs: bottom-sheet dismissible

---
+++ docs/examples/react/bottom-sheet/dismissible.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { ActionButton } from "seed-design/ui/action-button";
+import {
+  BottomSheetBody,
+  BottomSheetContent,
+  BottomSheetFooter,
+  BottomSheetRoot,
+  BottomSheetTrigger,
+} from "seed-design/ui/bottom-sheet";
+
+const BottomSheetDismissible = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <BottomSheetRoot open={open} onOpenChange={setOpen} dismissible={false}>
+      <BottomSheetTrigger asChild>
+        <ActionButton>Open</ActionButton>
+      </BottomSheetTrigger>
+      <BottomSheetContent title="제목" description="설명을 작성할 수 있어요">
+        {/* If you need to omit padding, pass px={0}. */}
+        <BottomSheetBody>Content</BottomSheetBody>
+        <BottomSheetFooter>
+          <ActionButton variant="neutralSolid" onClick={() => setOpen(false)}>
+            닫기
+          </ActionButton>
+        </BottomSheetFooter>
+      </BottomSheetContent>
+    </BottomSheetRoot>
+  );
+};
+
+export default BottomSheetDismissible;
