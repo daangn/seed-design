96573661b43e9f9865eee8366ca103d1a627b6f8
정현수 <hyeonsu.github@gmail.com>
2025-11-14 12:01:08 +0900

    feat: add bg/transparent token

--- packages/css/vars/color/bg.d.ts
+++ packages/css/vars/color/bg.d.ts
@@ -35,4 +35,5 @@ export declare const warningWeak = "var(--seed-color-bg-warning-weak)";
 export declare const warningSolid = "var(--seed-color-bg-warning-solid)";
 export declare const warningSolidPressed = "var(--seed-color-bg-warning-solid-pressed)";
 export declare const warningWeakPressed = "var(--seed-color-bg-warning-weak-pressed)";
+export declare const transparent = "var(--seed-color-bg-transparent)";
 export declare const transparentPressed = "var(--seed-color-bg-transparent-pressed)";
