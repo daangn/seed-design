77517f16067b72e4792e12105e93a7b0cc1d0292
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-11-24 18:37:18 +0900

    fix: fix avatars not being rendered properly (#973)
    * refactor: make constant type-safe
    
    * fix: apply gpu acceleration to avatar slots
    
    # Conflicts:
    #	packages/css/all.min.css
    
    * docs
    
    * refactor: remove unnecessary css declaration
    
    * docs

---
+++ docs/examples/react/avatar/badge.tsx
@@ -0,0 +1,46 @@
+import { IdentityPlaceholder } from "seed-design/ui/identity-placeholder";
+import { Avatar, AvatarBadge } from "seed-design/ui/avatar";
+import { Box, HStack } from "@seed-design/react";
+
+export default function() {
+  return (
+    <HStack gap="x4">
+      <Avatar
+        size="64"
+        badgeMask="circle"
+        src="https://avatars.githubusercontent.com/u/54893898?v=4"
+        fallback={<IdentityPlaceholder />}
+      >
+        <AvatarBadge asChild>
+          <Box bg="palette.green600" borderRadius="full" />
+        </AvatarBadge>
+      </Avatar>
+      <Avatar
+        size="64"
+        badgeMask="flower"
+        src="https://avatars.githubusercontent.com/u/54893898?v=4"
+        fallback={<IdentityPlaceholder />}
+      >
+        <AvatarBadge asChild>
+          <img
+            src="/flower_green_checkmark.svg"
+            alt="뱃지를 설명하는 대체 텍스트를 제공해야 합니다."
+          />
+        </AvatarBadge>
+      </Avatar>
+      <Avatar
+        size="64"
+        badgeMask="shield"
+        src="https://avatars.githubusercontent.com/u/54893898?v=4"
+        fallback={<IdentityPlaceholder />}
+      >
+        <AvatarBadge asChild>
+          <img
+            src="/shield_blue_checkmark.svg"
+            alt="뱃지를 설명하는 대체 텍스트를 제공해야 합니다."
+          />
+        </AvatarBadge>
+      </Avatar>
+    </HStack>
+  );
+}
