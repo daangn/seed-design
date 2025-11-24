ef350d18ae1085c47411a231116f8bf6324b26d3
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-11-24 11:20:40 +0900

    feat(rootage): add description slot to menu-sheet

--- packages/css/vars/component/menu-sheet-item.mjs
+++ packages/css/vars/component/menu-sheet-item.mjs
@@ -15,6 +15,12 @@ export const vars = {
         "fontSize": "var(--seed-font-size-t5)",
         "lineHeight": "var(--seed-line-height-t5)",
         "fontWeight": "var(--seed-font-weight-regular)"
+      },
+      "description": {
+        "fontSize": "var(--seed-font-size-t3)",
+        "lineHeight": "var(--seed-line-height-t3)",
+        "fontWeight": "var(--seed-font-weight-medium)",
+        "color": "var(--seed-color-fg-neutral-subtle)"
       }
     },
     "pressed": {
