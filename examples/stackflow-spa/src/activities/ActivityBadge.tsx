1ff424cf70cb062da71be91263797b7b503d3649
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-12-08 16:10:54 +0900

    docs

--- examples/stackflow-spa/src/activities/ActivityBadge.tsx
+++ examples/stackflow-spa/src/activities/ActivityBadge.tsx
@@ -12,6 +12,7 @@ import {
 import { AppScreen, AppScreenContent } from "seed-design/ui/app-screen";
 import { IconHouseLine } from "@karrotmarket/react-monochrome-icon";
 import { Badge, HStack, Text, VStack } from "@seed-design/react";
+import { badgeVariantMap } from "@seed-design/css/recipes/badge";
 
 declare module "@stackflow/config" {
   interface Register {
@@ -39,129 +40,27 @@ const ActivityBadge: StaticActivityComponentType<"ActivityBadge"> = () => {
         <VStack gap="x4" px="spacingX.globalGutter" py="x3" pb="safeArea">
           <Text textStyle="t1Bold">t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.</Text>
           <HStack gap="x2" wrap>
-            <VStack gap="x2">
-              <Badge size="medium" variant="outline" tone="neutral">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="outline" tone="brand">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="outline" tone="critical">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="outline" tone="informative">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="outline" tone="positive">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="outline" tone="warning">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-            </VStack>
-            <VStack gap="x2">
-              <Badge size="medium" variant="solid" tone="neutral">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="solid" tone="brand">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="solid" tone="critical">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="solid" tone="informative">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="solid" tone="positive">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="solid" tone="warning">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-            </VStack>
-            <VStack gap="x2">
-              <Badge size="medium" variant="weak" tone="neutral">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="weak" tone="brand">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="weak" tone="critical">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="weak" tone="informative">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="weak" tone="positive">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="medium" variant="weak" tone="warning">
-                t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-            </VStack>
+            {badgeVariantMap.variant.map((variant) => (
+              <VStack key={variant} gap="x2">
+                {badgeVariantMap.tone.map((tone) => (
+                  <Badge key={tone} size="medium" variant={variant} tone={tone}>
+                    t1 Est eiusmod sit do minim sunt incididunt aliqua et sit.
+                  </Badge>
+                ))}
+              </VStack>
+            ))}
           </HStack>
           <Text textStyle="t2Bold">t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.</Text>
           <HStack gap="x2" wrap>
-            <VStack gap="x2">
-              <Badge size="large" variant="outline" tone="neutral">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="outline" tone="brand">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="outline" tone="critical">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="outline" tone="informative">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="outline" tone="positive">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="outline" tone="warning">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-            </VStack>
-            <VStack gap="x2">
-              <Badge size="large" variant="solid" tone="neutral">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="solid" tone="brand">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="solid" tone="critical">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="solid" tone="informative">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="solid" tone="positive">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="solid" tone="warning">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-            </VStack>
-            <VStack gap="x2">
-              <Badge size="large" variant="weak" tone="neutral">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="weak" tone="brand">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="weak" tone="critical">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="weak" tone="informative">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="weak" tone="positive">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-              <Badge size="large" variant="weak" tone="warning">
-                t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
-              </Badge>
-            </VStack>
+            {badgeVariantMap.variant.map((variant) => (
+              <VStack key={variant} gap="x2">
+                {badgeVariantMap.tone.map((tone) => (
+                  <Badge key={tone} size="large" variant={variant} tone={tone}>
+                    t2 Est eiusmod sit do minim sunt incididunt aliqua et sit.
+                  </Badge>
+                ))}
+              </VStack>
+            ))}
           </HStack>
         </VStack>
       </AppScreenContent>
