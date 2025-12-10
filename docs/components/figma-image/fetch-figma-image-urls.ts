4933cba8b24ecdd0823780862f7cf12af3b23a01
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-12-10 20:03:27 +0900

    fix: fix max retrial

--- docs/components/figma-image/fetch-figma-image-urls.ts
+++ docs/components/figma-image/fetch-figma-image-urls.ts
@@ -4,7 +4,7 @@ import { FlatCache } from "flat-cache";
 import path from "node:path";
 
 const LOG_PREFIX = "\n[remark-figma-image]";
-const MAX_RETRIES = 30;
+const MAX_RETRIES = 7;
 const CACHE_TTL_MS = 14 * 24 * 60 * 60 * 1000; // 14 days
 const CACHE_ID = "urls";
 
