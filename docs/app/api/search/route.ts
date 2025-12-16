60a2b73035cd659a281fc5a7fb057083d3e1b9c3
정현수 <hyeonsu.github@gmail.com>
2025-12-16 15:53:45 +0900

    docs(api/search): add lynx source

--- docs/app/api/search/route.ts
+++ docs/app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { breezeSource, reactSource, source } from "@/app/source";
+import { breezeSource, reactSource, source, lynxSource } from "@/app/source";
 import { AdvancedIndex, createSearchAPI } from "fumadocs-core/search/server";
 import { tokenize } from "@/components/search/tokenizer";
 import { TAGS } from "@/app/api/search/constants";
@@ -42,6 +42,18 @@ export const { staticGET: GET } = createSearchAPI("advanced", {
           description: page.data.description,
           structuredData,
           tag: TAGS.breeze.value,
+          url: page.url,
+        } satisfies AdvancedIndex;
+      }),
+      ...lynxSource.getPages().map(async (page) => {
+        const { structuredData } = await page.data.load();
+
+        return {
+          id: page.url,
+          title: page.data.title,
+          description: page.data.description,
+          structuredData,
+          tag: TAGS.lynx.value,
           url: page.url,
         } satisfies AdvancedIndex;
       }),
