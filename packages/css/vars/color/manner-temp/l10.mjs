722bac79a4e4adbd1996dda976f2570e4c98e8bc
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-01-06 17:31:05 +0900

    feat: sort keys in rootage for maintainability (without any new/deleted keys)

--- packages/css/vars/color/manner-temp/l10.mjs
+++ packages/css/vars/color/manner-temp/l10.mjs
@@ -1,2 +1,2 @@
-export const text = "var(--seed-color-manner-temp-l10-text)";
 export const bg = "var(--seed-color-manner-temp-l10-bg)";
+export const text = "var(--seed-color-manner-temp-l10-text)";
--- packages/qvism-preset/src/vars/color/manner-temp/l5.mjs
+++ packages/qvism-preset/src/vars/color/manner-temp/l5.mjs
@@ -1,2 +1,2 @@
-export const text = "var(--seed-color-manner-temp-l5-text)";
 export const bg = "var(--seed-color-manner-temp-l5-bg)";
+export const text = "var(--seed-color-manner-temp-l5-text)";
