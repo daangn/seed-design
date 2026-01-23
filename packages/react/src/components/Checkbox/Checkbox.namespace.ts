341b98e09e28be458ea3d2f8fd19bb77308ce26f
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-01-23 18:34:09 +0900

    feat: add CheckboxGroup

--- packages/react/src/components/Checkbox/Checkbox.namespace.ts
+++ packages/react/src/components/Checkbox/Checkbox.namespace.ts
@@ -1,10 +1,12 @@
 export {
   CheckboxControl as Control,
+  CheckboxGroup as Group,
   CheckboxHiddenInput as HiddenInput,
   CheckboxIndicator as Indicator,
   CheckboxLabel as Label,
   CheckboxRoot as Root,
   type CheckboxControlProps as ControlProps,
+  type CheckboxGroupProps as GroupProps,
   type CheckboxHiddenInputProps as HiddenInputProps,
   type CheckboxIndicatorProps as IndicatorProps,
   type CheckboxLabelProps as LabelProps,
