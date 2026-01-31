01b5b710eb5981b98cc320a896f043f88cc07b74
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-01-31 21:33:49 +0900

    refactor: internalize useControllableState

---
+++ packages/react-headless/use-controllable-state/src/useControllableState.tsx
@@ -0,0 +1,97 @@
+// This code includes portions derived from radix-ui/primitives (https://github.com/radix-ui/primitives)
+// Used under the MIT License: https://opensource.org/licenses/MIT
+
+import * as React from "react";
+import { useLayoutEffect } from "@radix-ui/react-use-layout-effect";
+
+// Prevent bundlers from trying to optimize the import
+const useInsertionEffect: typeof useLayoutEffect =
+  (React as any)[" useInsertionEffect ".trim().toString()] || useLayoutEffect;
+
+type ChangeHandler<T> = (state: T) => void;
+type SetStateFn<T> = React.Dispatch<React.SetStateAction<T>>;
+
+interface UseControllableStateParams<T> {
+  prop?: T | undefined;
+  defaultProp: T;
+  onChange?: ChangeHandler<T>;
+  caller?: string;
+}
+
+export function useControllableState<T>({
+  prop,
+  defaultProp,
+  onChange = () => {},
+  caller,
+}: UseControllableStateParams<T>): [T, SetStateFn<T>] {
+  const [uncontrolledProp, setUncontrolledProp, onChangeRef] = useUncontrolledState({
+    defaultProp,
+    onChange,
+  });
+  const isControlled = prop !== undefined;
+  const value = isControlled ? prop : uncontrolledProp;
+
+  if (process.env.NODE_ENV !== "production") {
+    // biome-ignore lint/correctness/useHookAtTopLevel: OK to disable conditionally calling hooks here because they will always run consistently in the same environment. Bundlers should be able to remove the code block entirely in production.
+    const isControlledRef = React.useRef(prop !== undefined);
+
+    // biome-ignore lint/correctness/useHookAtTopLevel: same
+    React.useEffect(() => {
+      const wasControlled = isControlledRef.current;
+      if (wasControlled !== isControlled) {
+        const from = wasControlled ? "controlled" : "uncontrolled";
+        const to = isControlled ? "controlled" : "uncontrolled";
+        console.warn(
+          `${caller} is changing from ${from} to ${to}. Components should not switch from controlled to uncontrolled (or vice versa). Decide between using a controlled or uncontrolled value for the lifetime of the component.`,
+        );
+      }
+      isControlledRef.current = isControlled;
+    }, [isControlled, caller]);
+  }
+
+  const setValue = React.useCallback<SetStateFn<T>>(
+    (nextValue) => {
+      if (isControlled) {
+        const value = isFunction(nextValue) ? nextValue(prop) : nextValue;
+        if (value !== prop) {
+          onChangeRef.current?.(value);
+        }
+      } else {
+        setUncontrolledProp(nextValue);
+      }
+    },
+    [isControlled, prop, setUncontrolledProp, onChangeRef],
+  );
+
+  return [value, setValue];
+}
+
+function useUncontrolledState<T>({
+  defaultProp,
+  onChange,
+}: Omit<UseControllableStateParams<T>, "prop">): [
+  Value: T,
+  setValue: React.Dispatch<React.SetStateAction<T>>,
+  OnChangeRef: React.RefObject<ChangeHandler<T> | undefined>,
+] {
+  const [value, setValue] = React.useState(defaultProp);
+  const prevValueRef = React.useRef(value);
+
+  const onChangeRef = React.useRef(onChange);
+  useInsertionEffect(() => {
+    onChangeRef.current = onChange;
+  }, [onChange]);
+
+  React.useEffect(() => {
+    if (prevValueRef.current !== value) {
+      onChangeRef.current?.(value);
+      prevValueRef.current = value;
+    }
+  }, [value, prevValueRef]);
+
+  return [value, setValue, onChangeRef];
+}
+
+function isFunction(value: unknown): value is (...args: any[]) => any {
+  return typeof value === "function";
+}
