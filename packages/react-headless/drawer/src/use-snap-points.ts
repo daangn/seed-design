47a12fe5d8090c71236ea3bf486c09e50dad6222
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-02-02 20:15:37 +0900

    fix: fix build

--- packages/react-headless/drawer/src/use-snap-points.ts
+++ packages/react-headless/drawer/src/use-snap-points.ts
@@ -1,4 +1,4 @@
-import { useControllableState } from "@radix-ui/react-use-controllable-state";
+import { useControllableState } from "@seed-design/react-use-controllable-state";
 import React from "react";
 import { TRANSITIONS, VELOCITY_THRESHOLD } from "./constants";
 import { isVertical, set } from "./helpers";
