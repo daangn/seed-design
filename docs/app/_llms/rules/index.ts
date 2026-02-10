a3553a8594e0722d9e3822cdbe531d4fdd79b91c
정현수 <hyeonsu.github@gmail.com>
2026-02-10 17:20:01 +0900

    add type-table rule

--- docs/app/_llms/rules/index.ts
+++ docs/app/_llms/rules/index.ts
@@ -1,7 +1,8 @@
 import { codeBlockTabsRule } from "./codeblock-tabs-rule";
 import { componentExampleRule } from "./component-example-rule";
+import { typeTableRule } from "./type-table-rule";
 import type { Rule } from "./types";
 
-export const activeRules: Rule[] = [componentExampleRule, codeBlockTabsRule];
+export const activeRules: Rule[] = [componentExampleRule, codeBlockTabsRule, typeTableRule];
 
-export { codeBlockTabsRule, componentExampleRule };
+export { codeBlockTabsRule, componentExampleRule, typeTableRule };
