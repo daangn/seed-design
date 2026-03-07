d6b8025a3393ac1330cdb4feb90be2fdb261d6c0
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-03-07 16:00:53 +0900

    docs

--- docs/examples/react/content-placeholder/svg.tsx
+++ docs/examples/react/content-placeholder/svg.tsx
@@ -1,10 +1,23 @@
-import { IconPictureFill } from "@karrotmarket/react-monochrome-icon";
+import {
+  IconAppleFill,
+  IconDiamondFill,
+  IconSparkle2Fill,
+} from "@karrotmarket/react-monochrome-icon";
+import { HStack } from "@seed-design/react";
 import { ContentPlaceholder } from "seed-design/ui/content-placeholder";
 
 export default function ContentPlaceholderSvgExample() {
   return (
-    <ContentPlaceholder style={{ width: 120, height: 90 }}>
-      <IconPictureFill />
-    </ContentPlaceholder>
+    <HStack gap="x3">
+      <ContentPlaceholder style={{ width: 150, height: 150 }}>
+        <IconAppleFill />
+      </ContentPlaceholder>
+      <ContentPlaceholder style={{ width: 100, height: 150 }}>
+        <IconSparkle2Fill />
+      </ContentPlaceholder>
+      <ContentPlaceholder style={{ width: 200, height: 150 }}>
+        <IconDiamondFill />
+      </ContentPlaceholder>
+    </HStack>
   );
 }
