9499943f76b3ef56368102ecd74bc1d3fe95a3f7
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-03-20 12:08:21 +0900

    docs: update `앱` references
    @seed-design/css has updates but since they are jsdoc changes and don't have to trigger a release so not adding a changeset

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -4,9 +4,9 @@ export default async function HomePage() {
   return redirect("/docs");
 }
 
-export function generateMetadata() {
+export function generateMetadata(): Metadata {
   return {
     title: "SEED Design System",
-    description: "SEED 디자인 시스템은 당근 앱을 위한 통합된 디자인 언어입니다.",
-  } satisfies Metadata;
+    description: "SEED 디자인 시스템은 당근 제품을 위한 통합된 디자인 언어입니다.",
+  };
 }
