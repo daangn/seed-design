21ab3c875d8966c504fd274374a3da1ed5b4f235
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-03-24 16:05:56 +0900

    docs

--- docs/examples/react/responsive-design/responsive-grid.tsx
+++ docs/examples/react/responsive-design/responsive-grid.tsx
@@ -3,7 +3,7 @@ import { Box, Grid, Text, VStack } from "@seed-design/react";
 export default function ResponsiveGridExample() {
   return (
     <VStack gap="x2" align="center">
-      <Text textStyle="t3Medium">아래 Grid는 base breakpoint 별로 열 수가 다르게 구성됩니다.</Text>
+      <Text textStyle="t3Medium">아래 Grid는 breakpoint 별로 열 수가 다르게 구성됩니다.</Text>
       <Grid columns={{ base: 1, md: 2, lg: 4 }} gap={{ base: "x3", md: "x4" }}>
         <Box bg="bg.informativeWeak" color="fg.informativeContrast" padding="x3" borderRadius="r2">
           1
