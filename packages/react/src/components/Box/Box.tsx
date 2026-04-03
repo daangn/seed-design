614f791e549968ca657999d9d9b69481a9580542
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-04-03 19:59:08 +0900

    feat: breakpoint & responsive styling support (#1326)

--- packages/react/src/components/Box/Box.tsx
+++ packages/react/src/components/Box/Box.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import * as React from "react";
+import type { BreakpointThreshold } from "../../types/responsive";
 import { useStyleProps, type StyleProps } from "../../utils/styled";
 import { Slot } from "@radix-ui/react-slot";
 
@@ -7,17 +8,38 @@ export interface BoxProps extends StyleProps, Omit<React.HTMLAttributes<HTMLDivE
   as?: React.ElementType;
 
   asChild?: boolean;
+
+  hideFrom?: BreakpointThreshold;
 }
 
 export const Box = React.forwardRef<HTMLDivElement, BoxProps>((props, ref) => {
-  const { style, restProps } = useStyleProps(props);
+  const { hideFrom, ...rest } = props;
+  const { style, restProps } = useStyleProps(rest);
   const { as: Comp = "div", asChild = false, className, ...nativeProps } = restProps;
 
   if (asChild) {
     return (
-      <Slot ref={ref} className={clsx("seed-box", className)} style={style} {...nativeProps} />
+      <Slot
+        ref={ref}
+        className={clsx("seed-box", className)}
+        style={{
+          ...style,
+          ...(hideFrom && { [`--seed-box-display-${hideFrom}`]: "none" }),
+        }}
+        {...nativeProps}
+      />
     );
   }
 
-  return <Comp ref={ref} className={clsx("seed-box", className)} style={style} {...nativeProps} />;
+  return (
+    <Comp
+      ref={ref}
+      className={clsx("seed-box", className)}
+      style={{
+        ...style,
+        ...(hideFrom && { [`--seed-box-display-${hideFrom}`]: "none" }),
+      }}
+      {...nativeProps}
+    />
+  );
 });
