655834ecae0639a0e53948deaf2a0e430b144533
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-04-03 19:59:08 +0900

    feat: breakpoint & responsive styling support (#1326)

---
+++ packages/react/src/hooks/useBreakpointValue.ts
@@ -0,0 +1,33 @@
+import { type Breakpoint, breakpointNames } from "@seed-design/css/breakpoints";
+import { useMemo } from "react";
+
+import type { ResponsiveValue } from "../types/responsive";
+import { isResponsiveObject } from "../types/responsive";
+import { type UseBreakpointOptions, useBreakpoint } from "./useBreakpoint";
+
+export function resolveResponsiveValue<T>(
+  values: { [K in Breakpoint]?: T },
+  breakpoint: Breakpoint,
+): T | undefined {
+  const idx = breakpointNames.indexOf(breakpoint);
+  for (let i = idx; i >= 0; i--) {
+    const val = values[breakpointNames[i]];
+
+    if (val !== undefined) return val;
+  }
+
+  return undefined;
+}
+
+export function useBreakpointValue<T>(
+  values: ResponsiveValue<T>,
+  options?: UseBreakpointOptions,
+): T | undefined {
+  const breakpoint = useBreakpoint(options);
+
+  return useMemo(() => {
+    if (!isResponsiveObject(values)) return values;
+
+    return resolveResponsiveValue(values, breakpoint);
+  }, [values, breakpoint]);
+}
