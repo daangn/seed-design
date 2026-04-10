558b5bbd4c5cdb248e36aeef073c3d8a08bb39a9
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-04-10 14:35:36 +0900

    fix: add missing forwardRef

--- packages/react-headless/dismissible-layer/src/DismissibleLayer.tsx
+++ packages/react-headless/dismissible-layer/src/DismissibleLayer.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { composeRefs } from "@radix-ui/react-compose-refs";
 import { Slot } from "@radix-ui/react-slot";
+import { forwardRef } from "react";
 import { useDismissibleLayer, type UseDismissibleLayerOptions } from "./useDismissibleLayer";
 import { DismissibleParentContext } from "./layer-stack";
 
@@ -8,16 +10,16 @@ export interface DismissibleLayerProps extends UseDismissibleLayerOptions {
   children: React.ReactNode;
 }
 
-export const DismissibleLayer = (props: DismissibleLayerProps) => {
+export const DismissibleLayer = forwardRef<HTMLElement, DismissibleLayerProps>((props, ref) => {
   const { children, ...options } = props;
   const { dismissibleRef, dismissibleProps, layerNode } = useDismissibleLayer(options);
 
   return (
     <DismissibleParentContext.Provider value={layerNode}>
-      <Slot ref={dismissibleRef} {...dismissibleProps}>
+      <Slot ref={composeRefs(ref, dismissibleRef)} {...dismissibleProps}>
         {children}
       </Slot>
     </DismissibleParentContext.Provider>
   );
-};
+});
 DismissibleLayer.displayName = "DismissibleLayer";
