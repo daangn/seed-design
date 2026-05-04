d237f53d025b81867f573d4276416c43f020493f
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2026-05-04 19:24:46 +0900

    feat: add size=responsive variants (#1545)

--- packages/css/recipes/text-input.layered.mjs
+++ packages/css/recipes/text-input.layered.mjs
@@ -43,12 +43,20 @@ const compoundVariants = [
     "size": "medium"
   },
   {
+    "variant": "outline",
+    "size": "responsive"
+  },
+  {
     "variant": "underline",
     "size": "large"
   },
   {
     "variant": "underline",
     "size": "medium"
+  },
+  {
+    "variant": "underline",
+    "size": "responsive"
   }
 ];
 
@@ -59,7 +67,8 @@ export const textInputVariantMap = {
   ],
   "size": [
     "large",
-    "medium"
+    "medium",
+    "responsive"
   ]
 };
 
