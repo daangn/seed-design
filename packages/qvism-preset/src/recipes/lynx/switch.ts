4e156ec4d41ad54eb179fe54a4e0a8b424d1db47
정현수 <hyeonsu.github@gmail.com>
2026-05-11 15:44:19 +0900

    fix(lynx): align small switch label

--- packages/qvism-preset/src/recipes/lynx/switch.ts
+++ packages/qvism-preset/src/recipes/lynx/switch.ts
@@ -1,5 +1,4 @@
 import { switch as vars } from "../../vars/component";
-import { switchmark as markVars } from "../../vars/component";
 import { defineLynxSlotRecipe } from "../../utils/define-lynx";
 
 const switchRecipe = defineLynxSlotRecipe({
@@ -8,7 +7,7 @@ const switchRecipe = defineLynxSlotRecipe({
   base: {
     root: {
       display: "flex",
-      alignItems: "flex-start",
+      alignItems: "center",
       justifyContent: "space-between",
       position: "relative",
     },
@@ -25,36 +24,30 @@ const switchRecipe = defineLynxSlotRecipe({
         root: {
           minHeight: vars.size32.enabled.root.height,
           gap: vars.size32.enabled.root.gap,
-          "--switchmark-margin-top": `calc((${vars.size32.enabled.root.height} - ${markVars.size32.enabled.root.height}) / 2)`,
         },
         label: {
           fontSize: vars.size32.enabled.label.fontSize,
           lineHeight: vars.size32.enabled.label.lineHeight,
-          marginTop: `calc(${vars.size32.enabled.root.height} / 2 - ${vars.size32.enabled.label.lineHeight} / 2)`,
         },
       },
       24: {
         root: {
           minHeight: vars.size24.enabled.root.height,
           gap: vars.size24.enabled.root.gap,
-          "--switchmark-margin-top": `calc((${vars.size24.enabled.root.height} - ${markVars.size24.enabled.root.height}) / 2)`,
         },
         label: {
           fontSize: vars.size24.enabled.label.fontSize,
           lineHeight: vars.size24.enabled.label.lineHeight,
-          marginTop: `calc(${vars.size24.enabled.root.height} / 2 - ${vars.size24.enabled.label.lineHeight} / 2)`,
         },
       },
       16: {
         root: {
           minHeight: vars.size16.enabled.root.height,
           gap: vars.size16.enabled.root.gap,
-          "--switchmark-margin-top": `calc((${vars.size16.enabled.root.height} - ${markVars.size16.enabled.root.height}) / 2)`,
         },
         label: {
           fontSize: vars.size16.enabled.label.fontSize,
           lineHeight: vars.size16.enabled.label.lineHeight,
-          marginTop: `calc(${vars.size16.enabled.root.height} / 2 - ${vars.size16.enabled.label.lineHeight} / 2)`,
         },
       },
     },
