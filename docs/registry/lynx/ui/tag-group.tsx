11221b928d9bc0e685f7c06c18ad9b67bf0dfdf4
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2026-05-11 16:27:48 +0900

    docs(lynx): TagGroup snippet API 정렬 (#1580)

--- docs/registry/lynx/ui/tag-group.tsx
+++ docs/registry/lynx/ui/tag-group.tsx
@@ -1,16 +1,24 @@
 /**
  * @see https://seed-design.io/lynx/components/tag-group
  */
+import * as React from "@lynx-js/react";
 import { TagGroup as SeedTagGroup } from "@seed-design/lynx-react";
 
 export interface TagGroupRootProps extends SeedTagGroup.RootProps {}
 
 export const TagGroupRoot = SeedTagGroup.Root;
 
-export interface TagGroupItemProps extends SeedTagGroup.ItemProps {}
+export interface TagGroupItemProps extends Omit<SeedTagGroup.ItemProps, "children"> {
+  label: React.ReactNode;
+}
 
-export const TagGroupItem = SeedTagGroup.Item;
-
-export interface TagGroupItemLabelProps extends SeedTagGroup.ItemLabelProps {}
-
-export const TagGroupItemLabel = SeedTagGroup.ItemLabel;
+export const TagGroupItem = React.forwardRef<unknown, TagGroupItemProps>(
+  ({ label, ...props }, ref) => {
+    return (
+      <SeedTagGroup.Item {...props} ref={ref}>
+        <SeedTagGroup.ItemLabel>{label}</SeedTagGroup.ItemLabel>
+      </SeedTagGroup.Item>
+    );
+  },
+);
+TagGroupItem.displayName = "TagGroupItem";
--- examples/lynx-spa/src/seed-design/ui/tag-group.tsx
+++ examples/lynx-spa/src/seed-design/ui/tag-group.tsx
@@ -7,14 +7,28 @@
 /**
  * @see https://seed-design.io/lynx/components/tag-group
  */
-export {
-  TagGroupItem,
-  TagGroupItemLabel,
-  TagGroupRoot,
-  type TagGroupItemLabelProps,
-  type TagGroupItemProps,
-  type TagGroupRootProps,
-} from '@seed-design/lynx-react';
+import * as React from '@lynx-js/react';
+import { TagGroup as SeedTagGroup } from '@seed-design/lynx-react';
+
+export interface TagGroupRootProps extends SeedTagGroup.RootProps {}
+
+export const TagGroupRoot = SeedTagGroup.Root;
+
+export interface TagGroupItemProps
+  extends Omit<SeedTagGroup.ItemProps, 'children'> {
+  label: React.ReactNode;
+}
+
+export const TagGroupItem = React.forwardRef<unknown, TagGroupItemProps>(
+  ({ label, ...props }, ref) => {
+    return (
+      <SeedTagGroup.Item {...props} ref={ref}>
+        <SeedTagGroup.ItemLabel>{label}</SeedTagGroup.ItemLabel>
+      </SeedTagGroup.Item>
+    );
+  },
+);
+TagGroupItem.displayName = 'TagGroupItem';
 
 /**
  * This file is a snippet from SEED Design, helping you get started quickly with @seed-design/* packages.
