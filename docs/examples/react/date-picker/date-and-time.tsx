1308cace10008a386326400438b542400c70c887
iamchanii <26643843+iamchanii@users.noreply.github.com>
2026-08-03 09:31:57 +0900

    docs(date-picker): keep today fixed in time example

--- docs/examples/react/date-picker/date-and-time.tsx
+++ docs/examples/react/date-picker/date-and-time.tsx
@@ -11,18 +11,16 @@ import {
 } from "@seed-design/react";
 import * as React from "react";
 
+const TODAY: DatePickerDate = { year: 2026, month: 7, day: 30 };
+
 export default function DatePickerDateAndTime() {
-  const [date, setDate] = React.useState<DatePickerDate>({
-    year: 2026,
-    month: 7,
-    day: 30,
-  });
+  const [date, setDate] = React.useState<DatePickerDate>(TODAY);
   const [time, setTime] = React.useState<TimePickerValue>({ hour: 14, minute: 30 });
 
   return (
     <VStack gap="x4" align="center">
       <Box width="358px" maxWidth="100%">
-        <DatePicker today={date} value={date} onValueChange={setDate} />
+        <DatePicker today={TODAY} value={date} onValueChange={setDate} />
       </Box>
       <Box width="358px" maxWidth="100%">
         <TimePicker value={time} onValueChange={setTime} />
