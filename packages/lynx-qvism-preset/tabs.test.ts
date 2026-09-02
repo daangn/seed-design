0522a36b54ed7cd5895ce78b171062b61b2f65c9
iamchanii <26643843+iamchanii@users.noreply.github.com>
2026-09-02 17:30:01 +0900

    test(lynx-tabs): cover indicator transition override

--- packages/lynx-qvism-preset/tabs.test.ts
+++ packages/lynx-qvism-preset/tabs.test.ts
@@ -4,7 +4,7 @@ import tabs from "./src/recipes/tabs";
 import { tab as triggerVars, tablist as vars } from "./src/vars/component";
 
 describe("Lynx tabs recipe", () => {
-  it("uses a positive label color transition with a disabled override", () => {
+  it("defines an enabled label transition and disabled overrides", () => {
     expect(tabs.base["triggerLabel"]).toEqual({
       whiteSpace: "nowrap",
       color: triggerVars.base.enabled.label.color,
@@ -13,6 +13,9 @@ describe("Lynx tabs recipe", () => {
       transitionTimingFunction: vars.base.enabled.indicator.transformTimingFunction,
     });
     expect(tabs.variants["transitionEnabled"]["false"]["triggerLabel"]).toEqual({
+      transitionDuration: "0s",
+    });
+    expect(tabs.variants["transitionEnabled"]["false"]["indicator"]).toEqual({
       transitionDuration: "0s",
     });
   });
