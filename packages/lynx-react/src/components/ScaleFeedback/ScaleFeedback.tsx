aa37461dc5c40d3f9b5a49f63a3a84ded5e13811
정현수 <hyeonsu.github@gmail.com>
2026-09-04 10:14:30 +0900

    fix(lynx): forward scale feedback wrapper refs

--- packages/lynx-react/src/components/ScaleFeedback/ScaleFeedback.tsx
+++ packages/lynx-react/src/components/ScaleFeedback/ScaleFeedback.tsx
@@ -1,5 +1,6 @@
 import * as React from "@lynx-js/react";
 import { useScaleFeedback, type UseScaleFeedbackOptions } from "../../hooks/useScaleFeedback";
+import type { LynxViewRef } from "../../types";
 
 export interface ScaleFeedbackProps extends UseScaleFeedbackOptions {
   /** Content rendered inside the native element that receives Self Scale Feedback. */
@@ -9,12 +10,20 @@ export interface ScaleFeedbackProps extends UseScaleFeedbackOptions {
 /**
  * Applies Self Scale Feedback through a native Lynx view.
  */
-export function ScaleFeedback({ children, ...options }: ScaleFeedbackProps) {
-  const { scaleFeedbackTriggerProps, scaleFeedbackTargetProps } = useScaleFeedback(options);
+export const ScaleFeedback = React.forwardRef<unknown, ScaleFeedbackProps>(
+  ({ children, ...options }, ref) => {
+    const { scaleFeedbackTriggerProps, scaleFeedbackTargetProps } = useScaleFeedback(options);
 
-  return (
-    <view {...scaleFeedbackTriggerProps} {...scaleFeedbackTargetProps}>
-      {children}
-    </view>
-  );
-}
+    return (
+      <view
+        {...(ref ? { ref: ref as LynxViewRef } : {})}
+        {...scaleFeedbackTriggerProps}
+        {...scaleFeedbackTargetProps}
+      >
+        {children}
+      </view>
+    );
+  },
+);
+
+ScaleFeedback.displayName = "ScaleFeedback";
