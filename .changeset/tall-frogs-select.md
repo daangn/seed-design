39a8bf3e6f9fe06ee6693454b084c67d2343a45e
Chanhee Lee <26643843+iamchanii@users.noreply.github.com>
2026-09-07 11:49:32 +0900

    feat(lynx): add select component (#2174)
    * feat(lynx): add select component
    
    * commit bun.lock

---
+++ .changeset/tall-frogs-select.md
@@ -0,0 +1,6 @@
+---
+"@seed-design/lynx-css": minor
+"@seed-design/lynx-react": minor
+---
+
+Lynx Select 컴포넌트와 스타일 Recipe를 추가합니다.
---
+++ docs/examples/lynx/select/styles.ts
@@ -0,0 +1,5 @@
+import "@seed-design/lynx-css/base.css";
+import "@seed-design/lynx-css/recipes/select.css";
+import "@seed-design/lynx-css/recipes/select-trigger.css";
+import "@seed-design/lynx-css/recipes/select-item.css";
+import "./preview.css";
