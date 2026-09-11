b0586d230c9d7e9a8feee4ef93f8305959557933
iamchanii <26643843+iamchanii@users.noreply.github.com>
2026-09-11 12:30:55 +0900

    feat(lynx): add chip tabs with scroll alignment

--- packages/lynx-react/src/components/Tabs/index.ts
+++ packages/lynx-react/src/components/Tabs/index.ts
@@ -1,2 +1,18 @@
-export * from "./Tabs";
+export {
+  TabsCarousel,
+  TabsCarouselCamera,
+  TabsContent,
+  TabsIndicator,
+  TabsList,
+  TabsRoot,
+  TabsTrigger,
+  type TabsCarouselCameraProps,
+  type TabsCarouselProps,
+  type TabsContentProps,
+  type TabsIndicatorProps,
+  type TabsListProps,
+  type TabsRootProps,
+  type TabsTriggerProps,
+} from "./Tabs";
+
 export * as Tabs from "./Tabs.namespace";
