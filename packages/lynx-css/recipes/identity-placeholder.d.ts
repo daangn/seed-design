d4e8dcd318a040ac31a979b16d25f87895ad97f4
Chanhee Lee <26643843+iamchanii@users.noreply.github.com>
2026-09-11 16:18:13 +0900

    feat(lynx): add identity placeholder (#2209)
    * feat(lynx): add identity placeholder
    
    * fix(lynx): preserve identity placeholder aspect ratio

---
+++ packages/lynx-css/recipes/identity-placeholder.d.ts
@@ -0,0 +1,24 @@
+declare interface IdentityPlaceholderVariant {
+  /**
+  * @default "person"
+  */
+  identity: "person" | "business";
+}
+
+declare type IdentityPlaceholderVariantMap = {
+  [key in keyof IdentityPlaceholderVariant]: Array<IdentityPlaceholderVariant[key]>;
+};
+
+export declare type IdentityPlaceholderVariantProps = Partial<IdentityPlaceholderVariant>;
+
+export declare type IdentityPlaceholderSlotName = "root" | "image";
+
+export declare const identityPlaceholderVariantMap: IdentityPlaceholderVariantMap;
+
+export declare const identityPlaceholder: ((
+  props?: IdentityPlaceholderVariantProps,
+) => Record<IdentityPlaceholderSlotName, string>) & {
+  splitVariantProps: <T extends IdentityPlaceholderVariantProps>(
+    props: T,
+  ) => [IdentityPlaceholderVariantProps, Omit<T, keyof IdentityPlaceholderVariantProps>];
+}
